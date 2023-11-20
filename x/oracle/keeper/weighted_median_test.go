4374ca69dd8e2e52dad3c2d2d979d4d35c8d3a75
danflo27 <florentinodc@outlook.com>
2023-11-20 14:54:10 -0500

    add error messages for weighted median tests

--- x/oracle/keeper/weighted_median_test.go
+++ x/oracle/keeper/weighted_median_test.go
@@ -47,20 +47,20 @@ func (s *KeeperTestSuite) TestWeightedMedian() {
 	res, err := s.oracleKeeper.GetAggregatedReport(s.ctx, &types.QueryGetAggregatedReportRequest{QueryId: "query1"})
 	require.Nil(err)
 	//fmt.Println("REPORT 1: ", res.Report)
-	require.Equal(res.Report.QueryId, "query1")
-	require.Equal(res.Report.AggregateReporter, "reporter4")
-	require.Equal(res.Report.AggregateValue, "28")
-	require.Equal(res.Report.ReporterPower, int64(20))
+	require.Equal(res.Report.QueryId, "query1", "query id is not correct")
+	require.Equal(res.Report.AggregateReporter, "reporter4", "aggregate reporter is not correct")
+	require.Equal(res.Report.AggregateValue, "28", "aggregate value is not correct")
+	require.Equal(res.Report.ReporterPower, int64(20), "reporter power is not correct")
 	//  check list of reporters in the aggregate report
-	require.Equal(res.Report.Reporters[0].Reporter, "reporter1")
-	require.Equal(res.Report.Reporters[1].Reporter, "reporter2")
-	require.Equal(res.Report.Reporters[2].Reporter, "reporter3")
-	require.Equal(res.Report.Reporters[3].Reporter, "reporter4")
-	require.Equal(res.Report.Reporters[4].Reporter, "reporter5")
+	require.Equal(res.Report.Reporters[0].Reporter, "reporter1", "reporter is not correct")
+	require.Equal(res.Report.Reporters[1].Reporter, "reporter2", "reporter is not correct")
+	require.Equal(res.Report.Reporters[2].Reporter, "reporter3", "reporter is not correct")
+	require.Equal(res.Report.Reporters[3].Reporter, "reporter4", "reporter is not correct")
+	require.Equal(res.Report.Reporters[4].Reporter, "reporter5", "reporter is not correct")
 
 	weightedMean := float64((10*10)+(20*4)+(30*2)+(40*20)+(50*8)) / (10 + 4 + 2 + 20 + 8)
 	sum := ((10 * math.Pow(10-weightedMean, 2)) + (4 * math.Pow(20-weightedMean, 2)) + (2 * math.Pow(30-weightedMean, 2)) + (20 * math.Pow(40-weightedMean, 2)) + (8 * math.Pow(50-weightedMean, 2))) / (10 + 4 + 2 + 20 + 8)
-	require.Equal(res.Report.StandardDeviation, math.Sqrt(sum))
+	require.Equal(res.Report.StandardDeviation, math.Sqrt(sum), "std deviation is not correct")
 
 	// special case A -- lower weighted median and upper weighted median are equal, powers are equal
 	// calculates lower median
@@ -94,19 +94,19 @@ func (s *KeeperTestSuite) TestWeightedMedian() {
 	res, err = s.oracleKeeper.GetAggregatedReport(s.ctx, &types.QueryGetAggregatedReportRequest{QueryId: "query2"})
 	require.Nil(err)
 	//fmt.Println("REPORT 2: ", res.Report)
-	require.Equal(res.Report.QueryId, "query2")
-	require.Equal(res.Report.AggregateReporter, "reporter7")
-	require.Equal(res.Report.AggregateValue, "a")
-	require.Equal(res.Report.ReporterPower, int64(1))
+	require.Equal(res.Report.QueryId, "query2", "query id is not correct")
+	require.Equal(res.Report.AggregateReporter, "reporter7", "aggregate reporter is not correct")
+	require.Equal(res.Report.AggregateValue, "a", "aggregate value is not correct")
+	require.Equal(res.Report.ReporterPower, int64(1), "reporter power is not correct")
 	//  check list of reporters in the aggregate report
-	require.Equal(res.Report.Reporters[0].Reporter, "reporter6")
-	require.Equal(res.Report.Reporters[1].Reporter, "reporter7")
-	require.Equal(res.Report.Reporters[2].Reporter, "reporter8")
-	require.Equal(res.Report.Reporters[3].Reporter, "reporter9")
+	require.Equal(res.Report.Reporters[0].Reporter, "reporter6", "reporter is not correct")
+	require.Equal(res.Report.Reporters[1].Reporter, "reporter7", "reporter is not correct")
+	require.Equal(res.Report.Reporters[2].Reporter, "reporter8", "reporter is not correct")
+	require.Equal(res.Report.Reporters[3].Reporter, "reporter9", "reporter is not correct")
 
 	weightedMean = float64((10*1)+(10*1)+(20*1)+(20*1)) / (1 + 1 + 1 + 1)
 	sum = ((1 * math.Pow(10-weightedMean, 2)) + (1 * math.Pow(10-weightedMean, 2)) + (1 * math.Pow(20-weightedMean, 2)) + (1 * math.Pow(20-weightedMean, 2))) / (1 + 1 + 1 + 1)
-	require.Equal(res.Report.StandardDeviation, math.Sqrt(sum))
+	require.Equal(res.Report.StandardDeviation, math.Sqrt(sum), "std deviation is not correct")
 
 	// special case B -- lower weighted median and upper weighted median are equal, powers are not all equal
 	// calculates lower median
@@ -140,19 +140,19 @@ func (s *KeeperTestSuite) TestWeightedMedian() {
 	res, err = s.oracleKeeper.GetAggregatedReport(s.ctx, &types.QueryGetAggregatedReportRequest{QueryId: "query3"})
 	require.Nil(err)
 	//fmt.Println("REPORT 3: ", res.Report)
-	require.Equal(res.Report.QueryId, "query3")
-	require.Equal(res.Report.AggregateReporter, "reporter11")
-	require.Equal(res.Report.AggregateValue, "a")
-	require.Equal(res.Report.ReporterPower, int64(2))
+	require.Equal(res.Report.QueryId, "query3", "query id is not correct")
+	require.Equal(res.Report.AggregateReporter, "reporter11", "aggregate reporter is not correct")
+	require.Equal(res.Report.AggregateValue, "a", "aggregate value is not correct")
+	require.Equal(res.Report.ReporterPower, int64(2), "reporter power is not correct")
 	//  check list of reporters in the aggregate report
-	require.Equal(res.Report.Reporters[0].Reporter, "reporter10")
-	require.Equal(res.Report.Reporters[1].Reporter, "reporter11")
-	require.Equal(res.Report.Reporters[2].Reporter, "reporter12")
-	require.Equal(res.Report.Reporters[3].Reporter, "reporter13")
+	require.Equal(res.Report.Reporters[0].Reporter, "reporter10", "reporter is not correct")
+	require.Equal(res.Report.Reporters[1].Reporter, "reporter11", "reporter is not correct")
+	require.Equal(res.Report.Reporters[2].Reporter, "reporter12", "reporter is not correct")
+	require.Equal(res.Report.Reporters[3].Reporter, "reporter13", "reporter is not correct")
 
 	weightedMean = float64((10*1)+(10*2)+(20*1)+(20*2)) / (1 + 2 + 1 + 2)
 	sum = ((1 * math.Pow(10-weightedMean, 2)) + (2 * math.Pow(10-weightedMean, 2)) + (1 * math.Pow(20-weightedMean, 2)) + (2 * math.Pow(20-weightedMean, 2))) / (1 + 2 + 1 + 2)
-	require.Equal(res.Report.StandardDeviation, math.Sqrt(sum))
+	require.Equal(res.Report.StandardDeviation, math.Sqrt(sum), "std deviation is not correct")
 
 	// 5 reporters with even weights, should be equal to normal median
 	reports = []types.MicroReport{
@@ -191,19 +191,19 @@ func (s *KeeperTestSuite) TestWeightedMedian() {
 	res, err = s.oracleKeeper.GetAggregatedReport(s.ctx, &types.QueryGetAggregatedReportRequest{QueryId: "query4"})
 	require.Nil(err)
 	//fmt.Println("REPORT 4: ", res.Report)
-	require.Equal(res.Report.QueryId, "query4")
-	require.Equal(res.Report.AggregateReporter, "reporter16")
-	require.Equal(res.Report.AggregateValue, "1e")
-	require.Equal(res.Report.ReporterPower, int64(5))
+	require.Equal(res.Report.QueryId, "query4", "query id is not correct")
+	require.Equal(res.Report.AggregateReporter, "reporter16", "aggregate reporter is not correct")
+	require.Equal(res.Report.AggregateValue, "1e", "aggregate value is not correct")
+	require.Equal(res.Report.ReporterPower, int64(5), "reporter power is not correct")
 	//  check list of reporters in the aggregate report
-	require.Equal(res.Report.Reporters[0].Reporter, "reporter14")
-	require.Equal(res.Report.Reporters[1].Reporter, "reporter15")
-	require.Equal(res.Report.Reporters[2].Reporter, "reporter16")
-	require.Equal(res.Report.Reporters[3].Reporter, "reporter17")
-	require.Equal(res.Report.Reporters[4].Reporter, "reporter18")
+	require.Equal(res.Report.Reporters[0].Reporter, "reporter14", "reporter is not correct")
+	require.Equal(res.Report.Reporters[1].Reporter, "reporter15", "reporter is not correct")
+	require.Equal(res.Report.Reporters[2].Reporter, "reporter16", "reporter is not correct")
+	require.Equal(res.Report.Reporters[3].Reporter, "reporter17", "reporter is not correct")
+	require.Equal(res.Report.Reporters[4].Reporter, "reporter18", "reporter is not correct")
 
 	weightedMean = float64((10*5)+(20*5)+(30*5)+(40*5)+(50*5)) / (5 + 5 + 5 + 5 + 5)
 	sum = ((5 * math.Pow(10-weightedMean, 2)) + (5 * math.Pow(20-weightedMean, 2)) + (5 * math.Pow(30-weightedMean, 2)) + (5 * math.Pow(40-weightedMean, 2)) + (5 * math.Pow(50-weightedMean, 2))) / (5 + 5 + 5 + 5 + 5)
-	require.Equal(res.Report.StandardDeviation, math.Sqrt(sum))
+	require.Equal(res.Report.StandardDeviation, math.Sqrt(sum), "std deviation is not correct")
 
 }
--- x/oracle/keeper/weighted_mode_test.go
+++ x/oracle/keeper/weighted_mode_test.go
@@ -1,60 +1,107 @@
 package keeper_test
 
 import (
-	"math"
-
 	"github.com/tellor-io/layer/x/oracle/types"
 )
 
 func (s *KeeperTestSuite) TestWeightedMode() {
 	require := s.Require()
 
+	// normal scenario
 	// list of reports
 	reports := []types.MicroReport{
 		{
 			Reporter: "reporter1",
-			Value:    "a", // Hex value for 10
+			Value:    "aaa",
 			Power:    10,
 			QueryId:  "query1",
 		},
 		{
 			Reporter: "reporter2",
-			Value:    "14", // Hex value for 20
+			Value:    "aaa",
 			Power:    4,
 			QueryId:  "query1",
 		},
 		{
 			Reporter: "reporter3",
-			Value:    "1e", // Hex value for 30
+			Value:    "aaa",
 			Power:    2,
 			QueryId:  "query1",
 		},
 		{
 			Reporter: "reporter4",
-			Value:    "28", // Hex value for 40
+			Value:    "aaa",
 			Power:    20,
 			QueryId:  "query1",
 		},
 		{
 			Reporter: "reporter5",
-			Value:    "32", // Hex value for 50
+			Value:    "bbb",
 			Power:    8,
 			QueryId:  "query1",
 		},
 	}
 
-	s.oracleKeeper.WeightedMedian(s.ctx, reports)
+	s.oracleKeeper.WeightedMode(s.ctx, reports)
 	res, err := s.oracleKeeper.GetAggregatedReport(s.ctx, &types.QueryGetAggregatedReportRequest{QueryId: "query1"})
 	require.Nil(err)
-	require.Equal(res.Report.QueryId, "query1")
-	require.Equal(res.Report.AggregateReporter, "reporter4")
-	require.Equal(res.Report.AggregateValue, "28")
-	require.Equal(res.Report.ReporterPower, int64(20))
+	require.Equal(res.Report.QueryId, "query1", "query id is not correct")
+	require.Equal(res.Report.AggregateReporter, "reporter4", "aggregate reporter is not correct")
+	require.Equal(res.Report.AggregateValue, "aaa", "aggregate value is not correct")
+	require.Equal(res.Report.ReporterPower, int64(20), "aggregate reporter power is not correct")
 	//  check list of reporters in the aggregate report
-	require.Equal(res.Report.Reporters[0].Reporter, "reporter1")
-	require.Equal(res.Report.Reporters[1].Reporter, "reporter2")
+	require.Equal(res.Report.Reporters[0].Reporter, "reporter1", "reporter is not correct")
+	require.Equal(res.Report.Reporters[1].Reporter, "reporter2", "reporter is not correct")
+	require.Equal(res.Report.Reporters[2].Reporter, "reporter3", "reporter is not correct")
+	require.Equal(res.Report.Reporters[3].Reporter, "reporter4", "reporter is not correct")
+	require.Equal(res.Report.Reporters[4].Reporter, "reporter5", "reporter is not correct")
 
-	weightedMean := float64((10*10)+(20*4)+(30*2)+(40*20)+(50*8)) / (10 + 4 + 2 + 20 + 8)
-	sum := ((10 * math.Pow(10-weightedMean, 2)) + (4 * math.Pow(20-weightedMean, 2)) + (2 * math.Pow(30-weightedMean, 2)) + (20 * math.Pow(40-weightedMean, 2)) + (8 * math.Pow(50-weightedMean, 2))) / (10 + 4 + 2 + 20 + 8)
-	require.Equal(res.Report.StandardDeviation, math.Sqrt(sum))
+	// scenario where mode is not decided by most powerful reporter
+	reports = []types.MicroReport{
+		{
+			Reporter: "reporter6",
+			Value:    "ccc",
+			Power:    1,
+			QueryId:  "query2",
+		},
+		{
+			Reporter: "reporter7",
+			Value:    "ccc",
+			Power:    2,
+			QueryId:  "query2",
+		},
+		{
+			Reporter: "reporter8",
+			Value:    "ccc",
+			Power:    2,
+			QueryId:  "query2",
+		},
+		{
+			Reporter: "reporter9",
+			Value:    "ddd",
+			Power:    5,
+			QueryId:  "query2",
+		},
+		{
+			Reporter: "reporter10",
+			Value:    "ccc",
+			Power:    1,
+			QueryId:  "query2",
+		},
+	}
+
+	s.oracleKeeper.WeightedMode(s.ctx, reports)
+	res, err = s.oracleKeeper.GetAggregatedReport(s.ctx, &types.QueryGetAggregatedReportRequest{QueryId: "query1"})
+	require.Nil(err)
+	//require.Equal(res.Report.QueryId, "query1", "query id is not correct")
+	//require.Equal(res.Report.AggregateReporter, "reporter4", "aggregate reporter is not correct")
+	require.Equal(res.Report.AggregateValue, "ccc", "aggregate value is not correct")
+	//require.Equal(res.Report.ReporterPower, int64(20), "aggregate reporter power is not correct")
+	//  check list of reporters in the aggregate report
+	require.Equal(res.Report.Reporters[0].Reporter, "reporter1", "reporter is not correct")
+	require.Equal(res.Report.Reporters[1].Reporter, "reporter2", "reporter is not correct")
+	require.Equal(res.Report.Reporters[2].Reporter, "reporter3", "reporter is not correct")
+	require.Equal(res.Report.Reporters[3].Reporter, "reporter4", "reporter is not correct")
+	require.Equal(res.Report.Reporters[4].Reporter, "reporter5", "reporter is not correct")
+
 }
