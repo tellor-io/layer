9b33940e8d58c4eeac1bda2e5a428150ebce5342
akrem <akremstudy@gmail.com>
2023-12-18 15:01:29 -0500

    move interface registration

--- x/oracle/types/codec.go
+++ x/oracle/types/codec.go
@@ -5,6 +5,7 @@ import (
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
 )
 
 func RegisterCodec(cdc *codec.LegacyAmino) {
@@ -23,6 +24,9 @@ func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 	)
 	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgTip{},
+	)
+	registry.RegisterImplementations((*govtypes.Content)(nil),
+		&CycleListChangeProposal{},
 	)
 	// this line is used by starport scaffolding # 3
 
