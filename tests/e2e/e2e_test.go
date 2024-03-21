84edc23ca42d322d43889f44d436a33dcc91df66
danflo27 <florentinodc@outlook.com>
2024-03-21 16:12:00 -0400

    organize

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -26,7 +26,7 @@ func (s *E2ETestSuite) TestInitialMint() {
 	require.Equal(balance.Amount, math.NewInt(300*1e6))
 }
 
-func (s *E2ETestSuite) TestTransfer() {
+func (s *E2ETestSuite) TestTransferFromTeam() {
 	require := s.Require()
 
 	mintToTeamAcc := s.accountKeeper.GetModuleAddress(minttypes.MintToTeam)
@@ -34,14 +34,12 @@ func (s *E2ETestSuite) TestTransfer() {
 	balance := s.bankKeeper.GetBalance(s.ctx, mintToTeamAcc, s.denom)
 	require.Equal(balance.Amount, math.NewInt(300*1e6))
 
-	// create 10 accounts
+	// create 5 accounts
 	type Accounts struct {
 		PrivateKey secp256k1.PrivKey
 		Account    sdk.AccAddress
 	}
 	accounts := make([]Accounts, 0, 5)
-
-	// transfer from team to 10 accounts
 	for i := 0; i < 5; i++ {
 		privKey := secp256k1.GenPrivKey()
 		accountAddress := sdk.AccAddress(privKey.PubKey().Address())
@@ -87,100 +85,96 @@ func (s *E2ETestSuite) TestTransfer() {
 	require.Equal(s.bankKeeper.GetBalance(s.ctx, accounts[4].Account, s.denom).Amount, math.NewInt(1000))
 }
 
-func (s *E2ETestSuite) TestBecomeValidator() {
+func (s *E2ETestSuite) TestStakeTokens() {
+	// require := s.Require()
+
+	// accountAddrs, validatorAddrs := s.createValidators([]int64{10, 20, 30, 40, 50, 60, 70, 80, 90, 100})
+	// for i := range accountAddrs {
+	// 	validator, err := s.stakingKeeper.Validator(s.ctx, validatorAddrs[i])
+	// 	status := validator.GetStatus()
+	// 	require.Nil(err)
+	// 	require.Equal(stakingtypes.Bonded.String(), status.String())
+	// }
+
+	// // self-delegate
+	// val, err := s.stakingKeeper.GetValidator(s.ctx, validatorAddrs[0])
+	// require.Nil(err)
+	// power := val.GetConsensusPower(sdk.DefaultPowerReduction) // start with 10
+	// require.Equal(math.NewInt(10), math.NewInt(power))
+	// _, err = s.stakingKeeper.Delegate(s.ctx, accountAddrs[0], math.NewInt(10*1e6), stakingtypes.Unbonded, val, true) // delegate 10
+	// require.Nil(err)
+	// val, err = s.stakingKeeper.GetValidator(s.ctx, validatorAddrs[0])
+	// require.Nil(err)
+	// actualPower := val.GetConsensusPower(sdk.DefaultPowerReduction)                                                                  // 20
+	// expectedPower := math.NewInt(power).Add(math.NewInt(sdk.TokensToConsensusPower(math.NewInt(10*1e6), sdk.DefaultPowerReduction))) // 20
+	// require.Equal(expectedPower, math.NewInt(actualPower))
+
+	// // undelegate 1 of self-delegated stake
+	// power = val.GetConsensusPower(sdk.DefaultPowerReduction) // 20
+	// sharesAmount, err := s.stakingKeeper.ValidateUnbondAmount(
+	// 	s.ctx, accountAddrs[1], validatorAddrs[0], math.NewInt(10*1e5),
+	// )
+	// require.Nil(err)
+	// _, _, err = s.stakingKeeper.Undelegate(s.ctx, accountAddrs[0], validatorAddrs[0], sharesAmount) // undelegate 1
+	// require.Nil(err)
+
+	// unbondingAmount, err := s.stakingKeeper.GetDelegatorUnbonding(s.ctx, accountAddrs[0])
+	// fmt.Println("unbondingAmount: ", unbondingAmount)
+	// require.Nil(err)
+	// currentTime := s.ctx.BlockTime()
+	// fmt.Println("current time: ", currentTime)
+	// unbondingDelegation, err := s.stakingKeeper.GetAllUnbondingDelegations(s.ctx, accountAddrs[0])
+	// require.Nil(err)
+	// fmt.Println("unbondingDelegation: ", unbondingDelegation)
+
+	// val, err = s.stakingKeeper.GetValidator(s.ctx, validatorAddrs[0])
+	// require.Nil(err)
+	// actualPower = val.GetConsensusPower(sdk.DefaultPowerReduction)                                                                  // 19
+	// expectedPower = math.NewInt(power).Sub(math.NewInt(sdk.TokensToConsensusPower(math.NewInt(10*1e5), sdk.DefaultPowerReduction))) // 19
+	// require.Equal(expectedPower, math.NewInt(actualPower))
+
+	// // delegate from validator 1 to validator 0
+	// val, err = s.stakingKeeper.GetValidator(s.ctx, validatorAddrs[0])
+	// require.Nil(err)
+	// power = val.GetConsensusPower(sdk.DefaultPowerReduction)                                                         // 19
+	// _, err = s.stakingKeeper.Delegate(s.ctx, accountAddrs[1], math.NewInt(10*1e6), stakingtypes.Unbonded, val, true) // delegate 10
+	// require.Nil(err)
+	// val, err = s.stakingKeeper.GetValidator(s.ctx, validatorAddrs[0])
+	// require.Nil(err)
+	// actualPower = val.GetConsensusPower(sdk.DefaultPowerReduction)                                                                  // 29
+	// expectedPower = math.NewInt(power).Add(math.NewInt(sdk.TokensToConsensusPower(math.NewInt(10*1e6), sdk.DefaultPowerReduction))) // 29
+	// require.Equal(expectedPower, math.NewInt(actualPower))
+
+	// // undelegate from validator 1 to validator 0
+	// power = val.GetConsensusPower(sdk.DefaultPowerReduction) // 29
+	// sharesAmount, err = s.stakingKeeper.ValidateUnbondAmount(
+	// 	s.ctx, accountAddrs[1], validatorAddrs[0], math.NewInt(10*1e5),
+	// )
+	// require.Nil(err)
+	// // sharesAmount = math.LegacyNewDecFromInt(math.NewInt(1))
+	// _, _, err = s.stakingKeeper.Undelegate(s.ctx, accountAddrs[1], validatorAddrs[0], sharesAmount)
+	// require.Nil(err)
+
+	// unbondingAmount, err = s.stakingKeeper.GetDelegatorUnbonding(s.ctx, accountAddrs[1])
+	// fmt.Println("unbondingAmount: ", unbondingAmount)
+	// require.Nil(err)
+	// currentTime = s.ctx.BlockTime()
+	// fmt.Println("current time: ", currentTime)
+	// unbondingDelegation, err = s.stakingKeeper.GetAllUnbondingDelegations(s.ctx, accountAddrs[1])
+	// require.Nil(err)
+	// fmt.Println("unbondingDelegation: ", unbondingDelegation)
+
+	// val, err = s.stakingKeeper.GetValidator(s.ctx, validatorAddrs[0])
+	// require.Nil(err)
+	// actualPower = val.GetConsensusPower(sdk.DefaultPowerReduction) // should be 28 ?
+	// fmt.Println("actual power: ", actualPower)
+	// expectedPower = math.NewInt(power).Sub(math.NewInt(sdk.TokensToConsensusPower(math.NewInt(10*1e5), sdk.DefaultPowerReduction))) // 28
+	// fmt.Println("expected power: ", expectedPower)
+	// require.Equal(expectedPower, math.NewInt(actualPower))
 
 }
 
-// func (s *E2ETestSuite) TestStakeTokens() {
-// 	require := s.Require()
-
-// 	accountAddrs, validatorAddrs := s.createValidators([]int64{10, 20, 30, 40, 50, 60, 70, 80, 90, 100})
-// 	for i := range accountAddrs {
-// 		validator, err := s.stakingKeeper.Validator(s.ctx, validatorAddrs[i])
-// 		status := validator.GetStatus()
-// 		require.Nil(err)
-// 		require.Equal(stakingtypes.Bonded.String(), status.String())
-// 	}
-
-// 	// self-delegate
-// 	val, err := s.stakingKeeper.GetValidator(s.ctx, validatorAddrs[0])
-// 	require.Nil(err)
-// 	power := val.GetConsensusPower(sdk.DefaultPowerReduction) // start with 10
-// 	require.Equal(math.NewInt(10), math.NewInt(power))
-// 	_, err = s.stakingKeeper.Delegate(s.ctx, accountAddrs[0], math.NewInt(10*1e6), stakingtypes.Unbonded, val, true) // delegate 10
-// 	require.Nil(err)
-// 	val, err = s.stakingKeeper.GetValidator(s.ctx, validatorAddrs[0])
-// 	require.Nil(err)
-// 	actualPower := val.GetConsensusPower(sdk.DefaultPowerReduction)                                                                  // 20
-// 	expectedPower := math.NewInt(power).Add(math.NewInt(sdk.TokensToConsensusPower(math.NewInt(10*1e6), sdk.DefaultPowerReduction))) // 20
-// 	require.Equal(expectedPower, math.NewInt(actualPower))
-
-// 	// undelegate 1 of self-delegated stake
-// 	power = val.GetConsensusPower(sdk.DefaultPowerReduction) // 20
-// 	sharesAmount, err := s.stakingKeeper.ValidateUnbondAmount(
-// 		s.ctx, accountAddrs[1], validatorAddrs[0], math.NewInt(10*1e5),
-// 	)
-// 	require.Nil(err)
-// 	_, _, err = s.stakingKeeper.Undelegate(s.ctx, accountAddrs[0], validatorAddrs[0], sharesAmount) // undelegate 1
-// 	require.Nil(err)
-
-// 	unbondingAmount, err := s.stakingKeeper.GetDelegatorUnbonding(s.ctx, accountAddrs[0])
-// 	fmt.Println("unbondingAmount: ", unbondingAmount)
-// 	require.Nil(err)
-// 	currentTime := s.ctx.BlockTime()
-// 	fmt.Println("current time: ", currentTime)
-// 	unbondingDelegation, err := s.stakingKeeper.GetAllUnbondingDelegations(s.ctx, accountAddrs[0])
-// 	require.Nil(err)
-// 	fmt.Println("unbondingDelegation: ", unbondingDelegation)
-
-// 	val, err = s.stakingKeeper.GetValidator(s.ctx, validatorAddrs[0])
-// 	require.Nil(err)
-// 	actualPower = val.GetConsensusPower(sdk.DefaultPowerReduction)                                                                  // 19
-// 	expectedPower = math.NewInt(power).Sub(math.NewInt(sdk.TokensToConsensusPower(math.NewInt(10*1e5), sdk.DefaultPowerReduction))) // 19
-// 	require.Equal(expectedPower, math.NewInt(actualPower))
-
-// 	// delegate from validator 1 to validator 0
-// 	val, err = s.stakingKeeper.GetValidator(s.ctx, validatorAddrs[0])
-// 	require.Nil(err)
-// 	power = val.GetConsensusPower(sdk.DefaultPowerReduction)                                                         // 19
-// 	_, err = s.stakingKeeper.Delegate(s.ctx, accountAddrs[1], math.NewInt(10*1e6), stakingtypes.Unbonded, val, true) // delegate 10
-// 	require.Nil(err)
-// 	val, err = s.stakingKeeper.GetValidator(s.ctx, validatorAddrs[0])
-// 	require.Nil(err)
-// 	actualPower = val.GetConsensusPower(sdk.DefaultPowerReduction)                                                                  // 29
-// 	expectedPower = math.NewInt(power).Add(math.NewInt(sdk.TokensToConsensusPower(math.NewInt(10*1e6), sdk.DefaultPowerReduction))) // 29
-// 	require.Equal(expectedPower, math.NewInt(actualPower))
-
-// 	// undelegate from validator 1 to validator 0
-// 	power = val.GetConsensusPower(sdk.DefaultPowerReduction) // 29
-// 	sharesAmount, err = s.stakingKeeper.ValidateUnbondAmount(
-// 		s.ctx, accountAddrs[1], validatorAddrs[0], math.NewInt(10*1e5),
-// 	)
-// 	require.Nil(err)
-// 	// sharesAmount = math.LegacyNewDecFromInt(math.NewInt(1))
-// 	_, _, err = s.stakingKeeper.Undelegate(s.ctx, accountAddrs[1], validatorAddrs[0], sharesAmount)
-// 	require.Nil(err)
-
-// 	unbondingAmount, err = s.stakingKeeper.GetDelegatorUnbonding(s.ctx, accountAddrs[1])
-// 	fmt.Println("unbondingAmount: ", unbondingAmount)
-// 	require.Nil(err)
-// 	currentTime = s.ctx.BlockTime()
-// 	fmt.Println("current time: ", currentTime)
-// 	unbondingDelegation, err = s.stakingKeeper.GetAllUnbondingDelegations(s.ctx, accountAddrs[1])
-// 	require.Nil(err)
-// 	fmt.Println("unbondingDelegation: ", unbondingDelegation)
-
-// 	val, err = s.stakingKeeper.GetValidator(s.ctx, validatorAddrs[0])
-// 	require.Nil(err)
-// 	actualPower = val.GetConsensusPower(sdk.DefaultPowerReduction) // should be 28 ?
-// 	fmt.Println("actual power: ", actualPower)
-// 	expectedPower = math.NewInt(power).Sub(math.NewInt(sdk.TokensToConsensusPower(math.NewInt(10*1e5), sdk.DefaultPowerReduction))) // 28
-// 	fmt.Println("expected power: ", expectedPower)
-// 	require.Equal(expectedPower, math.NewInt(actualPower))
-
-// }
-
-func (s *E2ETestSuite) TestStaking() {
+func (s *E2ETestSuite) TestDelegate() {
 	require := s.Require()
 
 	// Create Validator Accounts
@@ -346,13 +340,13 @@ func (s *E2ETestSuite) TestValidateCycleList() {
 	require.Equal(ethQueryData[2:], thirdInCycle)
 }
 
-// currentTime := s.ctx.BlockTime()
-// fmt.Println(currentTime)
-// s.ctx = s.ctx.WithBlockTime(currentTime.Add(600 * time.Second)) // add 10 minutes
-// newTime := s.ctx.BlockTime()
-// fmt.Println(newTime)
-
 func (s *E2ETestSuite) TestSubmit() {
+	// currentTime := s.ctx.BlockTime()
+	// fmt.Println(currentTime)
+	// s.ctx = s.ctx.WithBlockTime(currentTime.Add(600 * time.Second)) // add 10 minutes
+	// newTime := s.ctx.BlockTime()
+	// fmt.Println(newTime)
+
 	// require := s.Require()
 	// _, msgServerOracle := s.oracleKeeper()
 	// require.NotNil(msgServerOracle)
