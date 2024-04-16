86b8a402fec2dd543aea6d2c01db2154252d685c
Cjpotter10 <caleb10joseph@gmail.com>
2024-04-16 16:39:05 -0500

    editted the start_one_node.sh script for starting a node in an aws ec2 isntance

---
+++ start_scripts/start_one_node_aws.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# clear the terminal
+clear
+
+# Stop execution if any command fails
+set -e
+
+KEYRING_BACKEND="test"
+PASSWORD="password"
+
+export LAYERD_NODE_HOME="$HOME/.layer/alice"
+
+# Remove old test chain data (if present)
+echo "Removing old test chain data..."
+rm -rf ~/.layer
+
+# Build layerd
+echo "Building layerd..."
+go build ./cmd/layerd
+
+# Initialize the chain
+echo "Initializing the chain..."
+./layerd init layer --chain-id layer
+
+# Initialize chain node with the folder for alice
+echo "Initializing chain node for alice..."
+./layerd init alicemoniker --chain-id layer --home ~/.layer/alice
+
+# Add a validator account for alice
+echo "Adding validator account for alice..."
+./layerd keys add alice --keyring-backend $KEYRING_BACKEND --home ~/.layer/alice
+
+# Update vote_extensions_enable_height in genesis.json for alice
+echo "Updating vote_extensions_enable_height in genesis.json for alice..."
+jq '.consensus.params.abci.vote_extensions_enable_height = "1"' ~/.layer/alice/config/genesis.json > temp.json && mv temp.json ~/.layer/alice/config/genesis.json
+
+# Create a tx to give alice loyas to stake
+echo "Adding genesis account for alice..."
+./layerd genesis add-genesis-account $(./layerd keys show alice -a --keyring-backend $KEYRING_BACKEND --home ~/.layer/alice) 10000000000000loya --keyring-backend $KEYRING_BACKEND --home ~/.layer/alice
+
+# Create a tx to stake some loyas for alice
+echo "Creating gentx for alice..."
+./layerd genesis gentx alice 1000000000000loya --chain-id layer --keyring-backend $KEYRING_BACKEND --home ~/.layer/alice --keyring-dir ~/.layer/alice
+
+# Add the transactions to the genesis block
+echo "Collecting gentxs..."
+./layerd genesis collect-gentxs --home ~/.layer/alice
+
+# Modify timeout_commit in config.toml for alice
+echo "Modifying timeout_commit in config.toml for alice..."
+sed -i '' 's/timeout_commit = "5s"/timeout_commit = "1s"/' ~/.layer/alice/config/config.toml
+
+
+# Modify cors to accept *
+echo "Modify cors to accept *"
+sed -i '' 's/^cors_allowed_origins = \[\]/cors_allowed_origins = \["\*"\]/g' ~/.layer/alice/config/config.toml
+sed -i '' 's/^cors_allowed_origins = \[\]/cors_allowed_origins = \["\*"\]/g' ~/.layer/config/config.toml
+
+
+# enable unsafe cors
+echo "Enable unsafe cors"
+sed -i '' 's/^cors_allowed_origins = \[\]/cors_allowed_origins = \["\*"\]/g' ~/.layer/alice/config/app.toml
+sed -i '' 's/^enable-unsafe-cors = false/enable-unsafe-cors = true/g' ~/.layer/alice/config/app.toml
+
+sed -i '' 's/^enabled-unsafe-cors = false/enabled-unsafe-cors = true/g' ~/.layer/config/app.toml
+sed -i '' 's/^enable-unsafe-cors = false/enable-unsafe-cors = true/g' ~/.layer/config/app.toml
+
+# set the external address for which to connect to
+echo "Setting external address to connect to for aws instance"
+sed -i '' 's/^external_address = ""/external_address = "52.201.190.14:26656"/g' ~/.layer/alice/config/config.toml
+sed -i '' 's/^external_address = ""/external_address = "52.201.190.14:26656"/g' ~/.layer/config/config.toml 
+
+# Modify keyring-backend in client.toml for alice
+echo "Modifying keyring-backend in client.toml for alice..."
+sed -i '' 's/^keyring-backend = "os"/keyring-backend = "test"/g' ~/.layer/alice/config/client.toml
+# update for main dir as well. why is this needed?
+sed -i '' 's/keyring-backend = "os"/keyring-backend = "test"/g' ~/.layer/config/client.toml
+
+echo "Starting chain for alice..."
+./layerd start --home $LAYERD_NODE_HOME --api.enable --api.swagger
