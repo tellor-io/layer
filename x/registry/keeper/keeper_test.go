f483808e5f867c7632c92d18df7e042bb03e899e
Nicholas Fett <nicholasfett@Nicholass-Air.hsd1.md.comcast.net>
2024-05-06 14:45:02 -0400

    someTests

--- x/registry/keeper/keeper_test.go
+++ x/registry/keeper/keeper_test.go
@@ -4,20 +4,58 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/cosmos/cosmos-sdk/types"
+	sdkTypes "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	"github.com/stretchr/testify/require"
+	"github.com/tellor-io/layer/x/registry/types"
 )
 
-func TestKeeper(t *testing.T) {
+func TestNewKeeper(t *testing.T) {
+	ms, ctx, k := setupMsgServer(t)
+	require.NotNil(t, ms)
+	require.NotNil(t, ctx)
+	require.NotNil(t, k)
+}
+
+func TestGetAuthority(t *testing.T) {
+	ms, ctx, k := setupMsgServer(t)
+	authority := authtypes.NewModuleAddress(govtypes.ModuleName).String()
+	queryType := "testQueryType"
+	spec := types.DataSpec{
+		DocumentHash:      "testHash",
+		ResponseValueType: "uint256",
+		AggregationMethod: "weighted-median",
+	}
+
+	// Register spec
+	registerSpecInput := &types.MsgRegisterSpec{
+		Registrar: "creator1",
+		QueryType: queryType,
+		Spec:      spec,
+	}
+	_, err := ms.RegisterSpec(ctx, registerSpecInput)
+	require.NoError(t, err)
+	_a := k.GetAuthority()
+	require.Equal(t, authority, _a)
+}
+
+func TestLogger(t *testing.T) {
 	ms, ctx, k := setupMsgServer(t)
 	require.NotNil(t, ms)
 	require.NotNil(t, ctx)
 	require.NotNil(t, k)
 
-	unwrappedCtx := types.UnwrapSDKContext(ctx)
+	unwrappedCtx := sdkTypes.UnwrapSDKContext(ctx)
 
 	// Logger
 	logger := k.Logger(unwrappedCtx)
 	loggerExpected := unwrappedCtx.Logger().With("module", fmt.Sprintf("x/%s", "registry"))
 	require.Equal(t, logger, loggerExpected, "logger does not match")
+}
+
+func TestSetHooks(t *testing.T) {
+}
+
+func TestHooks(t *testing.T) {
 }
