eea25fac3f47fbd72722ecfc163aa5f170551c9b
Cjpotter10 <caleb10joseph@gmail.com>
2024-05-09 18:42:44 -0500

    all tests passing but one for GetAggregateByHeight and I need to write one test for GetCurrentValueForQueryId. Should not take long

--- x/oracle/keeper/aggregate_test.go
+++ x/oracle/keeper/aggregate_test.go
@@ -1,6 +1,10 @@
 package keeper_test
 
 import (
+	"encoding/hex"
+	"fmt"
+	"math/big"
+	"strings"
 	"testing"
 	"time"
 
@@ -8,9 +12,11 @@ import (
 	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/stretchr/testify/mock"
 	"github.com/tellor-io/layer/testutil/sample"
 	layer "github.com/tellor-io/layer/types"
+	layertypes "github.com/tellor-io/layer/types"
 	minttypes "github.com/tellor-io/layer/x/mint/types"
 	"github.com/tellor-io/layer/x/oracle/types"
 )
@@ -171,36 +177,57 @@ func (s *KeeperTestSuite) TestGetTimestampAfter() {
 	}
 }
 
-func createMicroReportForQuery(reporterAdd, aggMethod, valueHex string, power int64, query types.QueryMeta, timestamp time.Time) types.MicroReport {
+func createMicroReportForQuery(reporterAdd, aggMethod, value string, power int64, timestamp time.Time) types.MicroReport {
 	return types.MicroReport{
 		Reporter:        reporterAdd,
 		Power:           power,
 		QueryId:         []byte("0x5c13cd9c97dbb98f2429c101a2a8150e6c7a0ddaff6124ee176a3a411067ded0"),
 		QueryType:       "SpotPrice",
 		AggregateMethod: aggMethod,
-		Value:           valueHex,
+		Value:           value,
 		Timestamp:       timestamp,
 		Cyclelist:       true,
 		BlockNumber:     10,
 	}
 }
 
+func encodeValue(number float64) string {
+	strNumber := fmt.Sprintf("%.18f", number)
+
+	parts := strings.Split(strNumber, ".")
+	if len(parts[1]) > 18 {
+		parts[1] = parts[1][:18]
+	}
+	truncatedStr := parts[0] + parts[1]
+
+	bigIntNumber := new(big.Int)
+	bigIntNumber.SetString(truncatedStr, 10)
+
+	uint256ABIType, _ := abi.NewType("uint256", "", nil)
+
+	arguments := abi.Arguments{{Type: uint256ABIType}}
+	encodedBytes, _ := arguments.Pack(bigIntNumber)
+
+	encodedString := hex.EncodeToString(encodedBytes)
+	return encodedString
+}
+
 func (s *KeeperTestSuite) TestSetAggregatedReport() {
 	timestamp := time.Now()
 	ctx := s.ctx.WithBlockTime(timestamp)
-	ctx = ctx.WithBlockHeight(10)
+
 	// setup
 	rep1 := sample.AccAddressBytes()
 	rep2 := sample.AccAddressBytes()
 	rep3 := sample.AccAddressBytes()
 	rep4 := sample.AccAddressBytes()
 
-	expiration := time.Now().Unix() - 20
+	expiration := timestamp.Unix() - 20
 
 	queryData := types.QueryMeta{
 		Id:                    1,
 		Amount:                math.NewInt(1 * 1e6),
-		Expiration:            time.Unix(expiration, expiration*time.Second.Nanoseconds()),
+		Expiration:            time.Unix(expiration, 0),
 		RegistrySpecTimeframe: 0,
 		HasRevealedReports:    true,
 		QueryId:               []byte("0x5c13cd9c97dbb98f2429c101a2a8150e6c7a0ddaff6124ee176a3a411067ded0"),
@@ -210,12 +237,12 @@ func (s *KeeperTestSuite) TestSetAggregatedReport() {
 	err := s.oracleKeeper.Query.Set(ctx, queryData.QueryId, queryData)
 	s.NoError(err)
 
-	val := "0x0000000000000000000000000000000000000000000000b4ed64f50fa9b7b8f2"
-
-	report_one := createMicroReportForQuery(rep1.String(), "weighted-median", val, 1000000000, queryData, time.Now())
-	report_two := createMicroReportForQuery(rep2.String(), "weighted-median", val, 1000000000, queryData, time.Now())
-	report_three := createMicroReportForQuery(rep3.String(), "weighted-median", val, 1000000000, queryData, time.Now())
-	report_four := createMicroReportForQuery(rep4.String(), "weighted-median", val, 1000000000, queryData, time.Now())
+	//val := "0x0000000000000000000000000000000000000000000000a3bc78653c25d80000"
+	val := encodeValue(1.00)
+	report_one := createMicroReportForQuery(rep1.String(), "weighted-median", val, 1000000000, time.Now())
+	report_two := createMicroReportForQuery(rep2.String(), "weighted-median", val, 1000000000, time.Now())
+	report_three := createMicroReportForQuery(rep3.String(), "weighted-median", val, 1000000000, time.Now())
+	report_four := createMicroReportForQuery(rep4.String(), "weighted-median", val, 1000000000, time.Now())
 
 	err = s.oracleKeeper.Reports.Set(ctx, collections.Join3(queryData.QueryId, rep1.Bytes(), queryData.Id), report_one)
 	s.NoError(err)
@@ -236,6 +263,8 @@ func (s *KeeperTestSuite) TestSetAggregatedReport() {
 	s.accountKeeper.On("GetModuleAccount", ctx, minttypes.TimeBasedRewards).Return(testModuleAccount)
 	s.bankKeeper.On("GetBalance", mock.Anything, mock.Anything, layer.BondDenom).Return(sdk.Coin{Amount: math.NewInt(1 * 1e6)})
 	s.bankKeeper.On("SendCoinsFromModuleToModule", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
+	s.reporterKeeper.On("DivvyingTips", mock.Anything, mock.Anything, mock.Anything).Return(nil)
+	s.reporterKeeper.On("AllocateTokensToReporter", mock.Anything, mock.Anything, mock.Anything).Return(nil)
 
 	err = s.oracleKeeper.SetAggregatedReport(ctx)
 	s.NoError(err)
@@ -258,7 +287,135 @@ func (s *KeeperTestSuite) TestSetAggregatedReport() {
 	s.Equal(false, res_query.HasRevealedReports)
 	s.Equal(math.ZeroInt(), res_query.Amount)
 
-	aggregate, err := s.oracleKeeper.Aggregates.Get(ctx, collections.Join(queryData.QueryId, timestamp.Unix()))
+	aggregate, err := s.oracleKeeper.GetCurrentValueForQueryId(ctx, []byte("0x5c13cd9c97dbb98f2429c101a2a8150e6c7a0ddaff6124ee176a3a411067ded0"))
 	s.NoError(err)
-	s.Equal(4, len(aggregate.Reporters))
+	s.NotEqual("", aggregate.AggregateValue)
+}
+
+func (s *KeeperTestSuite) TestSetAggregate() {
+	queryId := []byte("0x5c13cd9c97dbb98f2429c101a2a8150e6c7a0ddaff6124ee176a3a411067ded0")
+	err := s.oracleKeeper.Nonces.Set(s.ctx, queryId, 0)
+	s.NoError(err)
+	reporter := sample.AccAddressBytes()
+
+	timestamp := time.Now()
+	s.ctx = s.ctx.WithBlockTime(timestamp)
+	report := &types.Aggregate{
+		QueryId:           queryId,
+		AggregateValue:    encodeValue(96.50),
+		AggregateReporter: reporter.String(),
+		ReporterPower:     100000000,
+		StandardDeviation: 0.3,
+		Reporters:         []*types.AggregateReporter{{Reporter: reporter.String(), Power: 100000000}},
+		Flagged:           false,
+	}
+
+	err = s.oracleKeeper.SetAggregate(s.ctx, report)
+	s.NoError(err)
+
+	res, err := s.oracleKeeper.Aggregates.Get(s.ctx, collections.Join(queryId, timestamp.Unix()))
+	s.NoError(err)
+	s.Equal(encodeValue(96.50), res.AggregateValue)
+	s.Equal(int64(100000000), res.ReporterPower)
+}
+
+func (s *KeeperTestSuite) CreateReportAndReportersAtTimestamp(timestamp time.Time) (agg *types.Aggregate, queryId []byte, rep1 sdk.AccAddress, rep2 sdk.AccAddress, err error) {
+	rep1 = sample.AccAddressBytes()
+	rep2 = sample.AccAddressBytes()
+	queryId = []byte("0x5c13cd9c97dbb98f2429c101a2a8150e6c7a0ddaff6124ee176a3a411067ded0")
+
+	report := &types.Aggregate{
+		QueryId:           queryId,
+		AggregateValue:    encodeValue(96.50),
+		AggregateReporter: rep1.String(),
+		ReporterPower:     math.NewInt(200000000).Mul(layertypes.PowerReduction).Int64(),
+		StandardDeviation: 0.3,
+		Reporters:         []*types.AggregateReporter{{Reporter: rep1.String(), Power: 100000000}, {Reporter: rep2.String(), Power: 100000000}},
+		Flagged:           false,
+		Height:            10,
+	}
+
+	s.ctx = s.ctx.WithBlockTime(timestamp)
+	s.ctx = s.ctx.WithBlockHeight(10)
+	err = s.oracleKeeper.SetAggregate(s.ctx, report)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+
+	return report, queryId, rep1, rep2, nil
+}
+
+func (s *KeeperTestSuite) TestGetDataBeforePublic() {
+	reportedAt := time.Now()
+	aggregate, qId, _, _, err := s.CreateReportAndReportersAtTimestamp(reportedAt)
+	s.NoError(err)
+
+	jumpForward := reportedAt.Unix() + (60 * 5)
+	queryAt := time.Unix(jumpForward, 0)
+
+	s.ctx = s.ctx.WithBlockTime(queryAt)
+	retAggregate, err := s.oracleKeeper.GetDataBeforePublic(s.ctx, qId, queryAt)
+	s.NoError(err)
+	s.Equal(aggregate, retAggregate)
+}
+
+func (s *KeeperTestSuite) TestGetCurrentValueForQueryId() {
+	reportedAt := time.Now()
+	aggregate, qId, _, _, err := s.CreateReportAndReportersAtTimestamp(reportedAt)
+	s.NoError(err)
+
+	s.ctx = s.ctx.WithBlockTime(reportedAt)
+	retAggregate, err := s.oracleKeeper.GetCurrentValueForQueryId(s.ctx, qId)
+	s.NoError(err)
+	s.Equal(aggregate, retAggregate)
+}
+
+// STILL NEED TO FINISH THIS ONE
+func (s *KeeperTestSuite) TestGetAggregatedReportsByHeight() {
+	reportedAt := time.Now()
+	aggregate, _, _, _, err := s.CreateReportAndReportersAtTimestamp(reportedAt)
+	s.NoError(err)
+
+	s.ctx = s.ctx.WithBlockHeight(15)
+	aggregates := s.oracleKeeper.GetAggregatedReportsByHeight(s.ctx, int64(10))
+	s.NotEqual(0, len(aggregates))
+	s.Equal(aggregate, aggregates[0])
+}
+
+func (s *KeeperTestSuite) TestGetAggregateBefore() {
+	reportedAt := time.Now()
+	aggregate, qId, _, _, err := s.CreateReportAndReportersAtTimestamp(reportedAt)
+	s.NoError(err)
+
+	fastForward := reportedAt.Unix() + 240
+	queryAt := time.Unix(fastForward, 0)
+	s.ctx = s.ctx.WithBlockTime(queryAt)
+
+	retAgg, _, err := s.oracleKeeper.GetAggregateBefore(s.ctx, qId, queryAt)
+	s.NoError(err)
+	s.Equal(aggregate, retAgg)
+}
+
+func (s *KeeperTestSuite) TestGetAggregateByTimestamp() {
+	reportedAt := time.Now()
+	aggregate, qId, _, _, err := s.CreateReportAndReportersAtTimestamp(reportedAt)
+	s.NoError(err)
+
+	// fastForward := reportedAt.Unix() + 240
+	// queryAt := time.Unix(fastForward, 0)
+	s.ctx = s.ctx.WithBlockTime(reportedAt)
+
+	retAgg, err := s.oracleKeeper.GetAggregateByTimestamp(s.ctx, qId, reportedAt)
+	s.NoError(err)
+	s.Equal(aggregate, retAgg)
+}
+
+func (s *KeeperTestSuite) TestGetCurrentAggregateReport() {
+	reportedAt := time.Now()
+	aggregate, qId, _, _, err := s.CreateReportAndReportersAtTimestamp(reportedAt)
+	s.NoError(err)
+
+	retAgg, timestamp := s.oracleKeeper.GetCurrentAggregateReport(s.ctx, qId)
+	s.Equal(aggregate, retAgg)
+	s.Equal(reportedAt.Unix(), timestamp.Unix())
 }
