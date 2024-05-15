02b84e2e64110ac68ee5568a5a4cbfe633559243
Cjpotter10 <caleb10joseph@gmail.com>
2024-05-15 14:09:07 -0500

    corrected sed commands again

--- layer_scripts/join_chain_new_node_linux.sh
+++ layer_scripts/join_chain_new_node_linux.sh
@@ -113,8 +113,8 @@ curl tellornode.com:26657/genesis | jq '.result.genesis' > ~/.layer/$NODE_NAME/c
 ALICE_NODE_ID=0ae46718d9d95ab32bc12f5b4587a9a13dc85ca5
 ALICE_ID=$ALICE_NODE_ID@http://tellornode.com:26656
 
-sed -i '' 's/persistent_peers = ""/persistent_peers = "'$ALICE_ID'"/g' ~/.layer/$NODE_NAME/config/config.toml
-sed -i '' 's/seeds = ""/seeds = "'$ALICE_ID'"/g' ~/.layer/$NODE_NAME/config/config.toml
+sed -i '''s/persistent_peers = ""/persistent_peers = "'$ALICE_ID'"/g' ~/.layer/$NODE_NAME/config/config.toml
+sed -i '''s/seeds = ""/seeds = "'$ALICE_ID'"/g' ~/.layer/$NODE_NAME/config/config.toml
 
 
 echo "Starting chain for node..."
