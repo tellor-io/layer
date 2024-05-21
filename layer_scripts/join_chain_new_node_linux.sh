3f791769a1c9e74e0f6ce2266a7c075116356e06
Cjpotter10 <caleb10joseph@gmail.com>
2024-05-21 11:08:39 -0500

    trying another method of stripping quuotes from node id

--- layer_scripts/join_chain_new_node_linux.sh
+++ layer_scripts/join_chain_new_node_linux.sh
@@ -84,7 +84,7 @@ curl tellornode.com:26657/genesis | jq '.result.genesis' > ~/.layer/$NODE_NAME/c
 
 export QUOTED_TELLORNODE_ID="$(curl tellornode.com:26657/status | jq '.result.node_info.id')"
 #export TELLORNODE_ID=${QUOTED_TELLORNODE_ID//\"/}
-export TELLORNODE_ID="${QUOTED_TELLORNODE_ID:1:-1}"
+export TELLORNODE_ID=${echo "$QUOTED_TELLORNODE_ID" | tr -d "'\"" }
 echo "NODE ID: $TELLORNODE_ID"
 echo "Tellor node id: $TELLORNODE_ID"
 sed -i 's/seeds = ""/seeds = "'$TELLORNODE_ID'@tellornode.com:26656"/g' ~/.layer/$NODE_NAME/config/config.toml
