74d4e1ebe19544cf11fa4c31e984aea5726e7581
tkernell <tpkernell@gmail.com>
2024-07-12 15:34:36 -0500

    snapshot milliseconds

--- x/bridge/keeper/query_get_attestation_data_by_snapshot.go
+++ x/bridge/keeper/query_get_attestation_data_by_snapshot.go
@@ -34,11 +34,11 @@ func (q Querier) GetAttestationDataBySnapshot(goCtx context.Context, req *types.
 		return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("snapshot not found for snapshot %s", snapshot))
 	}
 	queryId := snapshotData.QueryId
-	timestampTime := time.Unix(snapshotData.Timestamp, 0)
+	timestampTime := time.UnixMilli(snapshotData.Timestamp)
 
 	aggReport, err := q.k.oracleKeeper.GetAggregateByTimestamp(ctx, queryId, timestampTime)
 	if err != nil {
-		return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("aggregate not found for queryId %s and timestamp %s", queryId, timestampTime))
+		return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("aggregate not found for queryId %s and timestamp %d", hex.EncodeToString(queryId), snapshotData.Timestamp))
 	}
 
 	queryIdStr := hex.EncodeToString(queryId)
