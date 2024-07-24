8c5d700322bbd0ef3a1e5d6bc7814587d9ac3645
Cjpotter10 <caleb10joseph@gmail.com>
2024-07-24 12:45:58 -0500

    rebased from main and fixed merge problems

--- x/oracle/abci.go
+++ x/oracle/abci.go
@@ -6,12 +6,11 @@ import (
 	"github.com/tellor-io/layer/x/oracle/keeper"
 )
 
+func BeginBlocker(ctx context.Context, k keeper.Keeper) error {
+	return k.SetAggregatedReport(ctx)
+}
+
 func EndBlocker(ctx context.Context, k keeper.Keeper) error {
 	// Rotate through the cycle list and set the current query index
-	err := k.RotateQueries(ctx)
-	if err != nil {
-		return err
-	}
-
-	return k.SetAggregatedReport(ctx)
+	return k.RotateQueries(ctx)
 }
