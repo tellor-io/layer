7b386009abfc71cd37936c87ba4ab3323220f87d
Cjpotter10 <caleb10joseph@gmail.com>
2024-07-26 11:05:40 -0500

    updated to use new node ip address instead of domain for p2p connection

--- layer_scripts/join_chain_new_node_linux.sh
+++ layer_scripts/join_chain_new_node_linux.sh
@@ -11,7 +11,7 @@ export LAYER_NODE_URL=tellorlayer.com
 export KEYRING_BACKEND="test"
 export NODE_MONIKER="billmoniker"
 export NODE_NAME="bill"
-export TELLORNODE_ID=d2ab6de0613631c6f6d6cca3c9bc76309a6ed04d
+export TELLORNODE_ID=f123e64bcb076508f3bdb19ceabc86a75ca1e330
 export LAYERD_NODE_HOME="$HOME/.layer/$NODE_NAME"
 
 
@@ -86,18 +86,18 @@ rm -f ~/.layer/config/genesis.json
 rm -f ~/.layer/$NODE_NAME/config/genesis.json
 # get genesis file from running node's rpc
 echo "Getting genesis from runnning node....."
-curl https://$LAYER_NODE_URL/rpc/genesis | jq '.result.genesis' > ~/.layer/config/genesis.json
-curl https://$LAYER_NODE_URL/rpc/genesis | jq '.result.genesis' > ~/.layer/$NODE_NAME/config/genesis.json
+curl https://tellorlayer.com/rpc/genesis | jq '.result.genesis' > ~/.layer/config/genesis.json
+curl https://tellorlayer.com/rpc/genesis | jq '.result.genesis' > ~/.layer/$NODE_NAME/config/genesis.json
 
-sed -i 's/seeds = ""/seeds = "'$TELLORNODE_ID'@https:\/\/'$LAYER_NODE_URL'/p2p"/g' ~/.layer/$NODE_NAME/config/config.toml
-sed -i 's/persistent_peers = ""/persistent_peers = "'$TELLORNODE_ID'@https:\/\/'$LAYER_NODE_URL':443/p2p"/g' ~/.layer/$NODE_NAME/config/config.toml
+sed -i 's/seeds = ""/seeds = "'$TELLORNODE_ID'@'$LAYER_NODE_URL':26656"/g' ~/.layer/$NODE_NAME/config/config.toml
+sed -i 's/persistent_peers = ""/persistent_peers = "'$TELLORNODE_ID'@'$LAYER_NODE_URL':26656"/g' ~/.layer/$NODE_NAME/config/config.toml
 
-echo "Path: $TELLORNODE_ID@$LAYER_NODE_URL/p2p"
+echo "Path: $TELLORNODE_ID@$LAYER_NODE_URL:26656"
 
 echo "Starting chain for node..."
 
 #./layerd start --home $LAYERD_NODE_HOME --api.enable --api.swagger --panic-on-daemon-failure-enabled=false --p2p.seeds "$TELLORNODE_ID@$LAYER_NODE_URL:26656"
-./layerd start --home $LAYERD_NODE_HOME --api.swagger --price-daemon-enabled=false --p2p.seeds "$TELLORNODE_ID@https://$LAYER_NODE_URL:443/p2p"
+./layerd start --home $LAYERD_NODE_HOME --api.swagger --price-daemon-enabled=false --p2p.seeds "$TELLORNODE_ID@$LAYER_NODE_URL:26656"
 # ./layerd start --home ~/.layer/bill --key-name bill --api.swagger --price-daemon-enabled=false --p2p.seeds "f123e64bcb076508f3bdb19ceabc86a75ca1e330@tellornode.com:26656"
 
 
