b720c7c886b6550ba30c67ef6e673e4ab7f13970
Dan F <florentinodc@outlook.com>
2024-08-16 13:07:50 -0400

    test: ante.go / extend_vote.go / proposal_handler.go tests  (#285)
    * vote ext suite setup
    
    * linting
    
    * suite problems
    
    * linting
    
    * voteext keyring testing
    
    * app test suites
    
    * proposal handler tests
    
    * process proposal rejected
    
    * process proposal accepted test
    
    * tests
    
    * remove printlns
    
    * revert back to main go.mod

---
+++ app/ante_test.go
@@ -0,0 +1,63 @@
+package app_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/tellor-io/layer/app"
+
+	"cosmossdk.io/x/tx/signing"
+
+	// antetestutil "cosmossdk.io/x/auth/ante/testutil"
+	"github.com/cosmos/cosmos-sdk/x/auth/ante"
+	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
+	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
+)
+
+func TestNewAnteHandler(t *testing.T) {
+	require := require.New(t)
+
+	// nil account keeper
+	options := app.HandlerOptions{
+		HandlerOptions: ante.HandlerOptions{
+			AccountKeeper: nil,
+		},
+	}
+	anteHandler, err := app.NewAnteHandler(options)
+	require.ErrorContains(err, "account keeper is required for ante builder")
+	require.Nil(anteHandler)
+
+	// nil bank keeper
+	options = app.HandlerOptions{
+		HandlerOptions: ante.HandlerOptions{
+			AccountKeeper: authkeeper.AccountKeeper{},
+			BankKeeper:    nil,
+		},
+	}
+	anteHandler, err = app.NewAnteHandler(options)
+	require.ErrorContains(err, "bank keeper is required for ante builder")
+	require.Nil(anteHandler)
+
+	// nil SignModeHandler
+	options = app.HandlerOptions{
+		HandlerOptions: ante.HandlerOptions{
+			AccountKeeper:   authkeeper.AccountKeeper{},
+			BankKeeper:      bankkeeper.BaseKeeper{},
+			SignModeHandler: nil,
+		},
+	}
+	anteHandler, err = app.NewAnteHandler(options)
+	require.ErrorContains(err, "sign mode handler is required for ante builder")
+	require.Nil(anteHandler)
+
+	options = app.HandlerOptions{
+		HandlerOptions: ante.HandlerOptions{
+			AccountKeeper:   authkeeper.AccountKeeper{},
+			BankKeeper:      bankkeeper.BaseKeeper{},
+			SignModeHandler: &signing.HandlerMap{},
+		},
+	}
+	anteHandler, err = app.NewAnteHandler(options)
+	require.NoError(err)
+	require.NotNil(anteHandler)
+}
