a63d0a97e3bbc3f1fc77179f8dc7b337810712bd
akrem <71235284+akremstudy@users.noreply.github.com>
2024-08-22 13:47:54 -0400

    Add module to upgrade handler (#297)
    * Update version and add fee param
    
    * err handle
    
    * add module to store upgrade struct
    
    * edit tag

--- app/app.go
+++ app/app.go
@@ -994,7 +994,7 @@ func (app *App) preBlocker(ph *ProposalHandler) func(sdk.Context, *abci.RequestF
 }
 
 func (app *App) RegisterUpgradeHandlers() {
-	const UpgradeName = "v0.4.1"
+	const UpgradeName = "v0.4.2"
 
 	app.UpgradeKeeper.SetUpgradeHandler(
 		UpgradeName,
@@ -1016,7 +1016,11 @@ func (app *App) RegisterUpgradeHandlers() {
 	}
 
 	if upgradeInfo.Name == UpgradeName && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
-		storeUpgrades := storetypes.StoreUpgrades{}
+		storeUpgrades := storetypes.StoreUpgrades{
+			Added: []string{
+				globalfee.ModuleName,
+			},
+		}
 
 		// configure store loader that checks if version == upgradeHeight and applies store upgrades
 		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
