f8640a0f2786d51d425a88a6e3f5bf90ad0921d1
akrem <71235284+akremstudy@users.noreply.github.com>
2024-08-23 13:46:54 -0400

    fix: remove unnecessary SetParams in upgrade handler (#299)
    * Update version and add fee param
    
    * err handle
    
    * add module to store upgrade struct
    
    * edit tag
    
    * remove setparams from upgrage handler

--- app/app.go
+++ app/app.go
@@ -77,7 +77,6 @@ import (
 	"cosmossdk.io/client/v2/autocli"
 	"cosmossdk.io/core/appmodule"
 	"cosmossdk.io/log"
-	"cosmossdk.io/math"
 	storetypes "cosmossdk.io/store/types"
 	"cosmossdk.io/x/evidence"
 	evidencekeeper "cosmossdk.io/x/evidence/keeper"
@@ -999,13 +998,6 @@ func (app *App) RegisterUpgradeHandlers() {
 	app.UpgradeKeeper.SetUpgradeHandler(
 		UpgradeName,
 		func(ctx context.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-			sdkctx := sdk.UnwrapSDKContext(ctx)
-			params := app.GlobalFeeKeeper.GetParams(sdkctx)
-			params.MinimumGasPrices = sdk.NewDecCoins(sdk.NewDecCoinFromDec(BondDenom, math.LegacyNewDecWithPrec(25, 4)))
-			err := app.GlobalFeeKeeper.SetParams(sdkctx, params)
-			if err != nil {
-				return nil, err
-			}
 			return app.ModuleManager().RunMigrations(ctx, app.Configurator(), fromVM)
 		},
 	)
