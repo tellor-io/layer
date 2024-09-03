843cbb4496802692be1a352b0f25d59fe339845e
tkernell <tpkernell@gmail.com>
2024-09-03 16:10:35 -0500

    Update client.go (#315)

--- daemons/token_bridge_feed/client/client.go
+++ daemons/token_bridge_feed/client/client.go
@@ -145,9 +145,7 @@ func (c *Client) InitializeDeposits() error {
 
 	c.ethClient = eclient
 
-
 	contractAddress := common.HexToAddress("0x717F9269032C25D91CcD92bADbdfcb32c30E9492")
-
 
 	bridgeContract, err := tokenbridge.NewTokenBridge(contractAddress, c.ethClient)
 	if err != nil {
