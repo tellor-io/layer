a1b1a97f5607973d3cfe6392ec75dd4874a89229
danflo27 <florentinodc@outlook.com>
2024-09-03 15:57:23 -0400

    registry coverage and linting

--- tests/integration/tipping_test.go
+++ tests/integration/tipping_test.go
@@ -2,20 +2,21 @@ package integration_test
 
 import (
 	"bytes"
-	"fmt"
 	"time"
 
-	"cosmossdk.io/math"
-	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/tellor-io/layer/testutil/sample"
 	"github.com/tellor-io/layer/utils"
 	"github.com/tellor-io/layer/x/oracle/keeper"
 	"github.com/tellor-io/layer/x/oracle/types"
 	reportertypes "github.com/tellor-io/layer/x/reporter/types"
+
+	"cosmossdk.io/math"
+
+	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-var pl = fmt.Println
+// var pl = fmt.Println
 
 // Test adding tip to a query that is already in cycle and not expired
 func (s *IntegrationTestSuite) TestTipQueryInCycle() {
@@ -99,5 +100,4 @@ func (s *IntegrationTestSuite) TestTipQueryInCycle() {
 	s.NoError(err)
 	s.Equal(math.NewInt(980_000), query.Amount)      // 2% burn
 	s.True(query.Expiration.Before(ctx.BlockTime())) // expired commit time window but still has 3 seconds left in reveal time window
-
 }
--- x/registry/keeper/query_params_test.go
+++ x/registry/keeper/query_params_test.go
@@ -11,10 +11,14 @@ import (
 
 func TestParamsQuery(t *testing.T) {
 	keeper, _, _, ctx := testkeeper.RegistryKeeper(t)
+	querier := rk.NewQuerier(keeper)
+
+	res, err := querier.Params(ctx, nil)
+	require.Error(t, err)
+	require.Nil(t, res)
 
 	params := types.DefaultParams()
 	require.NoError(t, keeper.SetParams(ctx, params))
-	querier := rk.NewQuerier(keeper)
 	response, err := querier.Params(ctx, &types.QueryParamsRequest{})
 	require.NoError(t, err)
 	require.Equal(t, &types.QueryParamsResponse{Params: params}, response)
