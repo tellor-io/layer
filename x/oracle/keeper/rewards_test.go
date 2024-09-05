8cf129cebc0cc9c82b2ff560757b81cdf5c9c2b8
akrem <akremstudy@gmail.com>
2024-09-05 11:41:03 -0400

    use Dec instead Int for reward calculation

--- x/oracle/keeper/rewards_test.go
+++ x/oracle/keeper/rewards_test.go
@@ -24,58 +24,72 @@ func TestCalculateRewardAmount(t *testing.T) {
 		reporterPowers []int64
 		totalPower     int64
 		reportsCount   int64
-		expectedAmount []math.Int
+		expectedAmount []math.LegacyDec
 	}{
 		{
 			name:           "Test all reporters report",
 			reporter:       []keeper.ReportersReportCount{{Power: 10, Reports: 1}, {Power: 20, Reports: 1}, {Power: 30, Reports: 1}, {Power: 40, Reports: 1}},
-			expectedAmount: []math.Int{math.NewInt(10), math.NewInt(20), math.NewInt(30), math.NewInt(40)},
+			expectedAmount: []math.LegacyDec{math.LegacyNewDec(10), math.LegacyNewDec(20), math.LegacyNewDec(30), math.LegacyNewDec(40)},
 			totalPower:     100, // 40 + 30 + 20 + 10
 
 		},
 		{
 			name:           "only 1 reports",
 			reporter:       []keeper.ReportersReportCount{{Power: 10, Reports: 1}, {Power: 20, Reports: 0}, {Power: 30, Reports: 0}, {Power: 40, Reports: 0}},
-			expectedAmount: []math.Int{math.NewInt(100), math.NewInt(0), math.NewInt(0), math.NewInt(0)},
+			expectedAmount: []math.LegacyDec{math.LegacyNewDec(100), math.LegacyNewDec(0), math.LegacyNewDec(0), math.LegacyNewDec(0)},
 			totalPower:     10,
 		},
 		{
 			name:           "only 1 and 3 reports one report, a single queryId",
 			reporter:       []keeper.ReportersReportCount{{Power: 10, Reports: 1}, {Power: 20, Reports: 0}, {Power: 30, Reports: 1}, {Power: 40, Reports: 0}},
-			expectedAmount: []math.Int{math.NewInt(25), math.NewInt(0), math.NewInt(75), math.NewInt(0)},
+			expectedAmount: []math.LegacyDec{math.LegacyNewDec(25), math.LegacyNewDec(0), math.LegacyNewDec(75), math.LegacyNewDec(0)},
 			totalPower:     40, // 30 + 10
 		},
 		{
 			name:           "all reporters report, a two queryIds",
 			reporter:       []keeper.ReportersReportCount{{Power: 10, Reports: 2}, {Power: 20, Reports: 2}, {Power: 30, Reports: 2}, {Power: 40, Reports: 2}},
-			expectedAmount: []math.Int{math.NewInt(10), math.NewInt(20), math.NewInt(30), math.NewInt(40)},
+			expectedAmount: []math.LegacyDec{math.LegacyNewDec(10), math.LegacyNewDec(20), math.LegacyNewDec(30), math.LegacyNewDec(40)},
 			totalPower:     200,
 		},
 		{
-			name:           "all reporters report single, and 1 reports two queryIds",
-			reporter:       []keeper.ReportersReportCount{{Power: 10, Reports: 2}, {Power: 20, Reports: 1}, {Power: 30, Reports: 1}, {Power: 40, Reports: 1}},
-			expectedAmount: []math.Int{math.NewInt(18), math.NewInt(18), math.NewInt(27), math.NewInt(36)},
-			totalPower:     110, // 40 + 30 + 20 + (10 * 2)
+			name:     "all reporters report single, and 1 reports two queryIds",
+			reporter: []keeper.ReportersReportCount{{Power: 10, Reports: 2}, {Power: 20, Reports: 1}, {Power: 30, Reports: 1}, {Power: 40, Reports: 1}},
+			expectedAmount: []math.LegacyDec{
+				// power*reports/totalPower*reward
+				math.LegacyNewDec(10 * 2).Quo(math.LegacyNewDec(110)).Mul(math.LegacyNewDec(100)),
+				math.LegacyNewDec(20).Quo(math.LegacyNewDec(110)).Mul(math.LegacyNewDec(100)),
+				math.LegacyNewDec(30).Quo(math.LegacyNewDec(110)).Mul(math.LegacyNewDec(100)),
+				math.LegacyNewDec(40).Quo(math.LegacyNewDec(110)).Mul(math.LegacyNewDec(100))},
+			totalPower: 110, // 40 + 30 + 20 + (10 * 2)
 		},
 		{
-			name:           "all reporters report single, 1 and 3 report a second queryId",
-			reporter:       []keeper.ReportersReportCount{{Power: 10, Reports: 2}, {Power: 20, Reports: 1}, {Power: 30, Reports: 2}, {Power: 40, Reports: 1}},
-			expectedAmount: []math.Int{math.NewInt(14), math.NewInt(14), math.NewInt(43), math.NewInt(29)},
-			totalPower:     140, // 40 + (30 * 2) + 20 + (10 * 2)
+			name:     "all reporters report single, 1 and 3 report a second queryId",
+			reporter: []keeper.ReportersReportCount{{Power: 10, Reports: 2}, {Power: 20, Reports: 1}, {Power: 30, Reports: 2}, {Power: 40, Reports: 1}},
+			expectedAmount: []math.LegacyDec{
+				math.LegacyMustNewDecFromStr("14.285714285714285700"),
+				math.LegacyMustNewDecFromStr("14.285714285714285700"),
+				math.LegacyMustNewDecFromStr("42.857142857142857100"),
+				math.LegacyMustNewDecFromStr("28.571428571428571400"),
+			},
+			totalPower: 140, // 40 + (30 * 2) + 20 + (10 * 2)
 		},
 	}
 	for _, tc := range testCases {
-		expectedTotalReward := math.ZeroInt()
+		expectedTotalReward := math.LegacyZeroDec()
+		totaldist := math.LegacyZeroDec()
 		t.Run(tc.name, func(t *testing.T) {
 			for i, r := range tc.reporter {
 				amount := keeper.CalculateRewardAmount(r.Power, int64(r.Reports), tc.totalPower, reward)
+				totaldist = totaldist.Add(amount)
 				require.Equal(t, amount, tc.expectedAmount[i])
+				if i == len(tc.reporter)-1 {
+					amount = amount.Add(math.LegacyNewDecFromInt(reward).Sub(totaldist))
+				}
 				expectedTotalReward = expectedTotalReward.Add(amount)
+
 			}
 		})
-		tolerance := reward.Sub(math.OneInt()) // when rounded
-		withinTolerance := expectedTotalReward.Equal(reward) || expectedTotalReward.Equal(tolerance)
-		require.True(t, withinTolerance, "reward amount should be within tolerance")
+		require.True(t, expectedTotalReward.Equal(math.LegacyNewDecFromInt(reward)), "reward amount should be within tolerance")
 	}
 }
 
@@ -111,8 +125,8 @@ func (s *KeeperTestSuite) TestAllocateRewards() {
 	require.NoError(err)
 	rep2Addr, err := sdk.AccAddressFromBech32(rep2)
 	require.NoError(err)
-	rk.On("DivvyingTips", ctx, rep1Addr, math.NewInt(50), int64(0)).Return(nil).Once()
-	rk.On("DivvyingTips", ctx, rep2Addr, math.NewInt(50), int64(0)).Return(nil).Once()
+	rk.On("DivvyingTips", ctx, rep1Addr, math.LegacyNewDec(50), int64(0)).Return(nil).Once()
+	rk.On("DivvyingTips", ctx, rep2Addr, math.LegacyNewDec(50), int64(0)).Return(nil).Once()
 	bk.On("SendCoinsFromModuleToModule", ctx, "oracle", "tips_escrow_pool", sdk.NewCoins(sdk.NewCoin("loya", reward))).Return(nil)
 	require.NoError(k.AllocateRewards(ctx, reporters, reward, types.ModuleName))
 }
@@ -162,7 +176,7 @@ func (s *KeeperTestSuite) TestAllocateTips() {
 	ctx := s.ctx
 
 	addr := sample.AccAddressBytes()
-	amount := math.NewInt(100)
+	amount := math.LegacyNewDec(100)
 	rk.On("DivvyingTips", ctx, addr, amount, ctx.BlockHeight()).Return(nil).Once()
 	require.NoError(k.AllocateTip(ctx, addr, amount, ctx.BlockHeight()))
 }
