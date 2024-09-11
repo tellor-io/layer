ef0af58b98528833133a9e4bd083fa9cceaa9132
tkernell <tpkernell@gmail.com>
2024-09-11 16:35:35 -0500

    change upgrade name

--- app/app.go
+++ app/app.go
@@ -993,7 +993,7 @@ func (app *App) preBlocker(ph *ProposalHandler) func(sdk.Context, *abci.RequestF
 }
 
 func (app *App) RegisterUpgradeHandlers() {
-	const UpgradeName = "v0.7.1-alpha1"
+	const UpgradeName = "v0.7.1-fix"
 
 	app.UpgradeKeeper.SetUpgradeHandler(
 		UpgradeName,
