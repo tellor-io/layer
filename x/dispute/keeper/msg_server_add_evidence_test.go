3c8db32306330cca61af58dce40646cf529a73d6
danflo27 <florentinodc@outlook.com>
2024-10-11 17:17:55 -0400

    MsgAddEvidence test

--- x/dispute/keeper/msg_server_add_evidence_test.go
+++ x/dispute/keeper/msg_server_add_evidence_test.go
@@ -1,34 +1,78 @@
 package keeper_test
 
-import "github.com/tellor-io/layer/x/dispute/types"
+import (
+	"time"
+
+	"github.com/tellor-io/layer/x/dispute/types"
+	oracletypes "github.com/tellor-io/layer/x/oracle/types"
+
+	"cosmossdk.io/collections"
+)
 
 func (s *KeeperTestSuite) TestAddEvidence() {
 	require := s.Require()
 	require.NotNil(s.msgServer)
+	ok := s.oracleKeeper
+	s.ctx = s.ctx.WithBlockTime(time.Now())
+	s.ctx = s.ctx.WithBlockHeight(10)
 
-	testCases := []struct {
-		name  string
-		msg   *types.MsgAddEvidence
-		err   bool
-		setup func()
-	}{
-		{
-			name: "empty message",
-			msg:  &types.MsgAddEvidence{},
-			err:  true,
-		},
+	// empty message
+	_, err := s.msgServer.AddEvidence(s.ctx, &types.MsgAddEvidence{})
+	require.Error(err)
+
+	// open dispute
+	dispute := s.dispute()
+	s.NoError(s.disputeKeeper.Disputes.Set(s.ctx, dispute.DisputeId, dispute))
+	disputedReporter := dispute.InitialEvidence.Reporter
+
+	// add evidence, evidence is not an aggregate
+	addtlReport := oracletypes.MicroReport{
+		Reporter:        disputedReporter,
+		Power:           100,
+		QueryType:       dispute.InitialEvidence.QueryType,
+		QueryId:         dispute.InitialEvidence.QueryId,
+		AggregateMethod: dispute.InitialEvidence.AggregateMethod,
+		Value:           "100",
+		Timestamp:       s.ctx.BlockTime(),
+		Cyclelist:       dispute.InitialEvidence.Cyclelist,
+		BlockNumber:     uint64(s.ctx.BlockHeight()),
 	}
-	for _, tc := range testCases {
-		s.Run(tc.name, func() {
-			if tc.setup != nil {
-				tc.setup()
-			}
-			_, err := s.msgServer.AddEvidence(s.ctx, tc.msg)
-			if tc.err {
-				require.Error(err)
-			} else {
-				require.NoError(err)
-			}
-		})
+	evidence := []*oracletypes.MicroReport{&addtlReport}
+	ok.On("FlagAggregateReport", s.ctx, addtlReport).Return(collections.ErrNotFound).Once()
+	_, err = s.msgServer.AddEvidence(s.ctx, &types.MsgAddEvidence{
+		DisputeId: dispute.DisputeId,
+		Reports:   evidence,
+	})
+	require.NoError(err)
+	// check dispute to see if report is in additional evidence
+	dispute, err = s.disputeKeeper.Disputes.Get(s.ctx, dispute.DisputeId)
+	require.NoError(err)
+	require.Equal(len(dispute.AdditionalEvidence), 1)
+
+	// add evidence that is an aggregate
+	addtlReport = oracletypes.MicroReport{
+		Reporter:        disputedReporter,
+		Power:           100,
+		QueryType:       dispute.InitialEvidence.QueryType,
+		QueryId:         dispute.InitialEvidence.QueryId,
+		AggregateMethod: dispute.InitialEvidence.AggregateMethod,
+		Value:           "200",
+		Timestamp:       s.ctx.BlockTime(),
+		Cyclelist:       dispute.InitialEvidence.Cyclelist,
+		BlockNumber:     uint64(s.ctx.BlockHeight()),
 	}
+	evidence = append(evidence, &addtlReport)
+	ok.On("FlagAggregateReport", s.ctx, *evidence[0]).Return(collections.ErrNotFound).Once()
+	ok.On("FlagAggregateReport", s.ctx, *evidence[1]).Return(nil).Once()
+	_, err = s.msgServer.AddEvidence(s.ctx, &types.MsgAddEvidence{
+		DisputeId: dispute.DisputeId,
+		Reports:   evidence,
+	})
+	require.NoError(err)
+	// check dispute to see if report is in additional evidence
+	dispute, err = s.disputeKeeper.Disputes.Get(s.ctx, dispute.DisputeId)
+	require.NoError(err)
+	require.Equal(len(dispute.AdditionalEvidence), 3) // first + 2 additional
+	// TODO: check that aggregate is flagged in integration/e2e suite
+
 }
