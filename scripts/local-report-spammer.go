5edb37d1625c43b961f3f9fddcc758a6c9ad6c20
Cjpotter10 <caleb10joseph@gmail.com>
2024-11-05 11:22:12 -0600

    added functions for creating new reporters

--- scripts/local-report-spammer.go
+++ scripts/local-report-spammer.go
@@ -4,27 +4,52 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
-	"strings"
 )
 
+var COMMAND_PATH = "/Users/caleb/layer/layerd"
+var LAYER_PATH = "/Users/caleb/.layer"
+
 func main() {
-	cmd := exec.Command("/Users/caleb/layer/layerd", "keys", "show", "faucet", "-a", "--keyring-backend", "test", "--home", "/Users/caleb/.layer")
+	cmd := exec.Command(COMMAND_PATH, "keys", "show", "faucet", "-a", "--keyring-backend", "test", "--home", LAYER_PATH)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Fatalf("cmd.Run() failed: %v\n", err)
 	}
 
-	convertedAddr := string(output[:])
+	fmt.Println("here")
+	addr := string(output[:len(output)-1])
 
-	addr := "tellor19d90wqftqx34khmln36zjdswm9p2aqawq2t3vp"
-	if strings.EqualFold(addr, convertedAddr) {
-		fmt.Println("these are literally the exact same and this makes no sense")
+	qd := "00000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000953706f745072696365000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000c0000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000004757364630000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000037573640000000000000000000000000000000000000000000000000000000000"
+	value := "0000000000000000000000000000000000000000000000000000000004a5ba50"
+
+	cmd = exec.Command(COMMAND_PATH, "tx", "oracle", "submit-value", addr, qd, value, "--from", addr, "--chain-id", "layertest-2", "--fees", "15loya", "--keyring-backend", "test", "--keyring-dir", LAYER_PATH, "--home", LAYER_PATH, "--node", "http://54.209.172.1:26657", "--yes")
+	output, _ = cmd.CombinedOutput()
+	fmt.Println(string(output))
+
+}
+
+func CreateNewAccountsAndFundReporters(numOfReporters int) (map[string]string, error) {
+	for i := 1; i <= numOfReporters; i++ {
+		key_name := fmt.Sprintf("test_reporter%d", i)
+		key_path := fmt.Sprintf("%s/%s", LAYER_PATH, key_name)
+		cmd := exec.Command(COMMAND_PATH, "add", key_name, "--keyring-backend", "test", "--home", key_path)
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Fatalf("creating key failed for %s: %v", key_name, err)
+		}
+		fmt.Println(string(output))
+
+		//./layerd keys add $name --keyring-backend $KEYRING_BACKEND --home ~/.layer/$name 2>&1 | tee $name-validator_keys.txt
 	}
-	fmt.Printf("Addr: %s, Converted Addr: %s\r", addr, convertedAddr)
-	// qd := "00000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000953706f745072696365000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000c0000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000004757364630000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000037573640000000000000000000000000000000000000000000000000000000000"
-	// value := "0000000000000000000000000000000000000000000000000000000004a5ba50"
+}
 
-	// cmd = exec.Command("/Users/caleb/layer/layerd", "tx", "oracle", "submit-value", addr, qd, value, "--from", addr, "--chain-id", "layertest-2", "--fees", "15loya", "--keyring-backend", "test", "--keyring-dir", "/Users/caleb/.layer", "--home", "/Users/caleb/.layer", "--node", "http://54.209.172.1:26657", "--yes")
-	// output, _ = cmd.CombinedOutput()
-	// fmt.Println(string(output))
+func GetAddressFromKeyName(key_name string) string {
+	cmd := exec.Command(COMMAND_PATH, "keys", "show", "faucet", "-a", "--keyring-backend", "test", "--home", LAYER_PATH)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("cmd.Run() failed: %v\n", err)
+	}
+
+	fmt.Println("here")
+	return string(output[:len(output)-1])
 }
