e56461e994461006e46ac81aebc6df8c754a6f0e
Cjpotter10 <caleb10joseph@gmail.com>
2024-11-07 09:56:45 -0600

    feat/use-events-in-daemon

--- daemons/reporter/client/reporter_monitors.go
+++ daemons/reporter/client/reporter_monitors.go
@@ -1,40 +1,99 @@
 package client
 
 import (
-	"bytes"
 	"context"
+	"encoding/json"
+	"net/url"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
 
+	"github.com/gorilla/websocket"
+	"github.com/tellor-io/layer/utils"
 	oracletypes "github.com/tellor-io/layer/x/oracle/types"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/query"
 )
 
+type Params struct {
+	Query string `json:"query"`
+}
+
+type WebsocketSubscribeRequest struct {
+	Jsonrpc string `json:"jsonrpc"`
+	Method  string `json:"method"`
+	Id      int    `json:"id"`
+	Params  Params `json:"params"`
+}
+
 func (c *Client) MonitorCyclelistQuery(ctx context.Context, wg *sync.WaitGroup) {
 	defer wg.Done()
-	prevQueryData := []byte{}
+	url := url.URL{Scheme: "ws", Host: "127.0.0.1:26657", Path: "/websocket"}
+	client, _, err := websocket.DefaultDialer.Dial(url.String(), nil)
+	if err != nil {
+		c.logger.Error("dial:", err)
+		return
+	}
+	defer client.Close()
+
+	subscribeReq := WebsocketSubscribeRequest{
+		Jsonrpc: "2.0",
+		Method:  "subscribe",
+		Id:      0,
+		Params:  Params{Query: "tm.event=\"rotating-cyclelist-with-existing-nontipped-query\""},
+	}
+	req, err := json.Marshal(&subscribeReq)
+	if err != nil {
+		c.logger.Error("Error marshalling request message: ", err)
+		return
+	}
+	err = client.WriteMessage(websocket.TextMessage, req)
+	if err != nil {
+		c.logger.Error("write:", err)
+		return
+	}
+
+	queryIdToQueryDataMap := CreateCyclelistQueryIdToQueryDataMap()
 
 	for {
-		querydata, querymeta, err := c.CurrentQuery(ctx)
+		_, message, err := client.ReadMessage()
 		if err != nil {
-			// log error
-			c.logger.Error("getting current query", "error", err)
-			time.Sleep(150 * time.Millisecond)
+			c.logger.Error("read:", err)
+			return
 		}
-		if bytes.Equal(querydata, prevQueryData) || commitedIds[querymeta.Id] {
-			time.Sleep(150 * time.Millisecond)
-			continue
+		var event sdk.Event
+		err = json.Unmarshal(message, &event)
+		if err != nil {
+			c.logger.Error("Unable to unmarshal read message: ", err)
+			c.logger.Info("Response data: ", message)
+			return
 		}
 
-		go func(ctx context.Context, qd []byte, qm *oracletypes.QueryMeta) {
-			err := c.GenerateAndBroadcastSpotPriceReport(ctx, querydata, qm)
+		qidAttribute, ok := event.GetAttribute("query_id")
+		if !ok {
+			c.logger.Error("No attribute found for query_id")
+			return
+		}
+		query_id := qidAttribute.Value
+		qd := queryIdToQueryDataMap[query_id]
+
+		idAttribute, ok := event.GetAttribute("New QueryMeta Id")
+		if !ok {
+			c.logger.Error("no next id found in rotatequeries event")
+		}
+		nextId, err := strconv.Atoi(idAttribute.Value)
+		if err != nil {
+			c.logger.Error("error converting id attribute to int: ", err)
+			return
+		}
+		go func(query_data []byte, querymetaId uint64) {
+			err := c.GenerateAndBroadcastCyclelistReport(ctx, query_data, querymetaId)
 			if err != nil {
-				c.logger.Error("Generating CycleList message", "error", err)
+				c.logger.Error("Error broadcasting cyclelist message: ", err)
 			}
-		}(ctx, querydata, querymeta)
-		time.Sleep(250 * time.Millisecond)
+		}(qd, uint64(nextId))
 	}
 }
 
@@ -106,4 +165,18 @@ func (c *Client) MonitorForTippedQueries(ctx context.Context, wg *sync.WaitGroup
 		}
 		localWG.Wait()
 	}
+}
+
+func CreateCyclelistQueryIdToQueryDataMap() map[string][]byte {
+	cyclelistQueryInfoMap := make(map[string][]byte, 0)
+	eth_usd_query_data, _ := utils.QueryBytesFromString("00000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000953706f745072696365000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000c0000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000003657468000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000037573640000000000000000000000000000000000000000000000000000000000")
+	cyclelistQueryInfoMap["83a7f3d48786ac2667503a61e8c415438ed2922eb86a2906e4ee66d9a2ce4992"] = eth_usd_query_data
+
+	btc_usd_query_data, _ := utils.QueryBytesFromString("00000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000953706f745072696365000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000c0000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000003627463000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000037573640000000000000000000000000000000000000000000000000000000000")
+	cyclelistQueryInfoMap["a6f013ee236804827b77696d350e9f0ac3e879328f2a3021d473a0b778ad78ac"] = btc_usd_query_data
+
+	trb_usd_query_data, _ := utils.QueryBytesFromString("00000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000953706f745072696365000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000c0000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000003747262000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000037573640000000000000000000000000000000000000000000000000000000000")
+	cyclelistQueryInfoMap["5c13cd9c97dbb98f2429c101a2a8150e6c7a0ddaff6124ee176a3a411067ded0"] = trb_usd_query_data
+
+	return cyclelistQueryInfoMap
 }
