8aae459206b03aae9fa958f070fbdf627e304cd2
danflo27 <florentinodc@outlook.com>
2024-11-18 10:51:39 -0500

    escalated dispute test in progress

--- e2e/escalated_dispute_test.go
+++ e2e/escalated_dispute_test.go
@@ -18,10 +18,6 @@ import (
 )
 
 func TestEscalatedDispute(t *testing.T) {
-	f, err := interchaintest.CreateLogFile("spinup_test.log")
-	require.NoError(t, err)
-	defer f.Close()
-
 	require := require.New(t)
 	ctx := context.Background()
 	layer := e2e.LayerSpinup(t)
@@ -31,19 +27,21 @@ func TestEscalatedDispute(t *testing.T) {
 	require.NoError(err)
 
 	// make a user account with 100 trb
-	user1 := interchaintest.GetAndFundTestUsers(t, ctx, "user1", math.NewInt(100*1e6), layer)[0]
-	user1Address := user1.FormattedAddress()
-	fmt.Println("User1 address: ", user1Address)
+	user1 := interchaintest.GetAndFundTestUsers(t, ctx, "user1", math.NewInt(900_000*1e6), layer)[0]
+	user1Addr := user1.FormattedAddress()
+	fmt.Println("User1 address: ", user1Addr)
+
+	// subscribe to events ?
 
 	// submit minting proposal and vote yes on it from all validators
 	require.NoError(e2e.TurnOnMinting(ctx, layer, validatorI))
 
 	// custom gov params set voting period to 15s, block time is 5s
-	// wait 4 blocks to be safe (3 sometimes works)
+	// waits 4 blocks (3 sometimes works)
 	require.NoError(testutil.WaitForBlocks(ctx, 4, validatorI))
 	result, err := layer.GovQueryProposal(ctx, 1)
 	require.NoError(err)
-	// proposal passes
+
 	fmt.Println("Proposal status: ", result.Status.String())
 	require.Equal(result.FinalTallyResult.Yes.String(), "10000000000000")
 	require.Equal(result.FinalTallyResult.No.String(), "0")
@@ -59,46 +57,55 @@ func TestEscalatedDispute(t *testing.T) {
 	// user tips random amount (<1 trb + 1 loya) for LTC/USD
 	randomTipInt := rand.Int63n(1000000) + 1
 	randomTip := sdk.NewCoin("loya", math.NewInt(randomTipInt))
-	stdout, _, err := validatorI.Exec(ctx, validatorI.TxCommand("user1", "oracle", "tip", user1Address, ltcusdQData, randomTip.String(), "--keyring-dir", "/var/cosmos-chain/layer-1"), validatorI.Chain.Config().Env)
+	fmt.Println("ltc/usd tip: ", randomTip.String())
+
+	stdout, _, err := validatorI.Exec(ctx, validatorI.TxCommand("user1", "oracle", "tip", user1Addr, ltcusdQData, randomTip.String(), "--keyring-dir", "/var/cosmos-chain/layer-1"), validatorI.Chain.Config().Env)
 	require.NoError(err)
-	fmt.Println("LTC/USD tip: ", randomTip.String())
 	txHash, err = e2e.GetTxHashFromExec(stdout)
-	fmt.Println("TX HASH(user tips ltc/usd): ", txHash)
+	fmt.Println("TX HASH (user tips ltc/usd): ", txHash)
 	require.NoError(testutil.WaitForBlocks(ctx, 1, validatorI))
 
 	// validator/reporter submits good value for LTC/USD
 	ltcusdValue := layerutil.EncodeValue(75.98)
 	valI, err := layer.StakingQueryValidator(ctx, valAddress)
 	require.NoError(err)
-	expectedPower := valI.Tokens
+	expectedPower := valI.Tokens // loya
+
 	txHash, err = validatorI.ExecTx(ctx, "validator", "oracle", "submit-value", valAccAddress, ltcusdQData, ltcusdValue, "--keyring-dir", "/var/cosmos-chain/layer-1")
 	require.NoError(err)
 	fmt.Println("TX HASH (user reports LTC/USD): ", txHash)
-	// wait 1 more block for aggregation
-	require.NoError(testutil.WaitForBlocks(ctx, 1, validatorI))
+	// require.NoError(testutil.WaitForBlocks(ctx, 1, validatorI))
 
-	// make sure all is square on aggregate repor\
+	// make sure all is square on aggregate report
 	ltcusdReport, _, err := validatorI.ExecQuery(ctx, "oracle", "get-reportsby-reporter", valAccAddress)
 	require.NoError(err)
 	var microReports e2e.ReportsResponse
 	require.NoError(json.Unmarshal(ltcusdReport, &microReports))
+
 	require.Equal(microReports.MicroReports[0].Reporter, valAccAddress)
 	require.Equal(microReports.MicroReports[0].Value, ltcusdValue)
 	require.Equal(microReports.MicroReports[0].AggregateMethod, "weighted-median")
-	require.Equal(microReports.MicroReports[0].Power, expectedPower.QuoRaw(layertypes.PowerReduction.Int64()).String())
+	require.Equal(microReports.MicroReports[0].Power, expectedPower.QuoRaw(layertypes.PowerReduction.Int64()).String()) // power is in trb
 	require.Equal(microReports.MicroReports[0].QueryType, "SpotPrice")
 	// require.Equal(microReports.MicroReports[0].QueryID, ltcusdQId) // GVhdkSr7cjeOOYanpT8erh+655LNF+HQ3wY2gTJoI64= expected ?
-	expectedTxInfo, _, err := validatorI.ExecQuery(ctx, "tx", txHash)
-	require.NotNil(expectedTxInfo)
-	require.NoError(err)
-	// TODO: unmarshal tx query
+	txResp, err := validatorI.TxHashToResponse(ctx, txHash)
+	fmt.Println("txResp.Events: ", txResp.Events[len(txResp.Events)-1])
+	fmt.Println("txResp.Logs: ", txResp.Logs)
+	fmt.Println("txResp.Tx: ", txResp.Tx)
+	fmt.Println("txResp.Height: ", txResp.Height)
+	// txQuery, _, err := validatorI.ExecQuery(ctx, "tx", txHash)
+	// require.NotNil(txQuery)
+	// require.NoError(err)
+	// fmt.Println("tx query: ", string(txQuery))
 	// require.Equal(microReports.MicroReports[0].BlockNumber, blockNum)
 	// require.Equal(microReports.MicroReports[0].Timestamp, timestamp)
 
 	// user opens warning dispute on report
 	bz, err := json.Marshal(microReports.MicroReports[0])
 	require.NoError(err)
-	txHash, err = validatorI.ExecTx(ctx, "user1", "dispute", "propose-dispute", string(bz), "warning", "500000000000loya", "true", "--keyring-dir", "/var/cosmos-chain/layer-1", "--gas", "1000000", "--fees", "1000000loya")
+
+	txHash, err = validatorI.ExecTx(ctx, user1Addr, "dispute", "propose-dispute", string(bz), "warning", "500000000000loya", "false", "--keyring-dir", "/var/cosmos-chain/layer-1", "--gas", "1000000", "--fees", "1000000loya")
 	require.NoError(err)
-	fmt.Println("TX HASH(user opens warning dispute on report): ", txHash)
+	fmt.Println("TX HASH (user opens warning dispute on report): ", txHash)
+
 }
