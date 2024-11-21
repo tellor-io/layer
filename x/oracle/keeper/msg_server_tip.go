cd574c1f78e779ed099bda6501dbbc63715e5229
Cjpotter10 <caleb10joseph@gmail.com>
2024-11-21 11:46:31 -0600

    added querymeta id as an attribute in tip_added event

--- x/oracle/keeper/msg_server_tip.go
+++ x/oracle/keeper/msg_server_tip.go
@@ -101,6 +101,7 @@ func (k msgServer) Tip(goCtx context.Context, msg *types.MsgTip) (*types.MsgTipR
 			sdk.NewAttribute("query_id", hex.EncodeToString(queryId)),
 			sdk.NewAttribute("tipper", tipper.String()),
 			sdk.NewAttribute("amount", tip.Amount.String()),
+			sdk.NewAttribute("querymeta_id", strconv.Itoa(int(query.Id)))
 		),
 	})
 	return &types.MsgTipResponse{}, nil
