a662ce83471d1a3ed763740e4ab53031e98c2acb
danflo27 <florentinodc@outlook.com>
2024-11-21 09:50:19 -0500

    re-add max commision rate check

--- x/reporter/keeper/msg_server.go
+++ x/reporter/keeper/msg_server.go
@@ -58,9 +58,9 @@ func (k msgServer) CreateReporter(goCtx context.Context, msg *types.MsgCreateRep
 		return nil, errors.New("address already exists")
 	}
 
-	// if msg.CommissionRate.GT(math.NewUint(1e6)) {
-	// 	return nil, errors.New("commission rate must be below 1000000 as that is a 100 percent commission rate")
-	// }
+	if msg.CommissionRate.GT(math.LegacyNewDec(100)) {
+		return nil, errors.New("commission rate must be LTE 100 as that is a 100 percent commission rate")
+	}
 	// set the reporter and set the self selector
 	if err := k.Keeper.Reporters.Set(goCtx, addr.Bytes(), types.NewReporter(msg.CommissionRate, msg.MinTokensRequired)); err != nil {
 		return nil, err
