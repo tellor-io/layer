16bbdce97e6a9a9f264be2198ab3231075b3646a
akrem <akremstudy@gmail.com>
2024-11-24 23:05:40 -0500

    reduce for loops in WeightedMode

--- x/oracle/keeper/weighted_mode.go
+++ x/oracle/keeper/weighted_mode.go
@@ -13,34 +13,22 @@ func (k Keeper) WeightedMode(ctx context.Context, reports []types.MicroReport, m
 
 	var modeReport types.MicroReport
 	var modeReporters []*types.AggregateReporter
-	maxWeight := uint64(0)
-	maxFrequency := 0
-	var mode string
-	frequencyMap := make(map[string]int)
-
 	var totalReporterPower uint64
-	// populate frequency map
-	for _, r := range reports {
-		modeReporters = append(modeReporters, &types.AggregateReporter{Reporter: r.Reporter, Power: r.Power, BlockNumber: r.BlockNumber})
-		entries := r.Power
-		for i := uint64(0); i < entries; i++ {
-			frequencyMap[r.Value]++
-		}
-		totalReporterPower += r.Power
-	}
-
-	// find the max frequency
-	for value, frequency := range frequencyMap {
-		if frequency > maxFrequency {
-			maxFrequency = frequency
-			mode = value
-		}
-	}
-
-	// set mode report from most powerful reporter who submitted mode value
 	var modeReportIndex uint64
+	var maxFrequency uint64
+	var maxWeight uint64
+	// populate frequency map
+	frequencyMap := make(map[string]uint64)
 	for i, r := range reports {
-		if mode == r.Value {
+		modeReporters = append(
+			modeReporters,
+			&types.AggregateReporter{
+				Reporter: r.Reporter, Power: r.Power, BlockNumber: r.BlockNumber,
+			})
+		frequencyMap[r.Value] += r.Power
+		totalReporterPower += r.Power
+		if frequencyMap[r.Value] > maxFrequency {
+			maxFrequency = frequencyMap[r.Value]
 			if r.Power > maxWeight {
 				maxWeight = r.Power
 				modeReport = r
