f1b9c1d86a8cac32d233687fdf01f0d8bbd2db8c
akrem <71235284+akremstudy@users.noreply.github.com>
2024-12-18 13:11:29 -0500

    mv conditional to top

--- app/app.go
+++ app/app.go
@@ -1008,26 +1008,27 @@ func (app *App) preBlocker(ph *ProposalHandler) func(ctx sdk.Context, _ *abci.Re
 		if err != nil {
 			return nil, err
 		}
-		bondedPoolBal := app.BankKeeper.GetBalance(ctx, authtypes.NewModuleAddress(stakingtypes.BondedPoolName), "loya")
-		vals, err := app.StakingKeeper.GetValidators(ctx, 7) // only 6 validators exist
-		if err != nil {
-			return nil, err
-		}
-		bondedtotal := math.ZeroInt()
-		notbondedtotal := math.ZeroInt()
-		for _, v := range vals {
-			if v.IsBonded() {
-				bondedtotal = bondedtotal.Add(v.Tokens)
-			} else {
-				notbondedtotal = notbondedtotal.Add(v.Tokens)
+		if ctx.BlockHeight() == 1062219+1 { // todo: figure out height
+			bondedPoolBal := app.BankKeeper.GetBalance(ctx, authtypes.NewModuleAddress(stakingtypes.BondedPoolName), "loya")
+			vals, err := app.StakingKeeper.GetValidators(ctx, 7) // only 6 validators exist
+			if err != nil {
+				return nil, err
 			}
-		}
-		// transfer amount should be 1950000000
-		// "spendable balance 97171770000loya is smaller than 99121770000loya
-		transferAmt := bondedPoolBal.Amount.Sub(bondedtotal)
-		if transferAmt.Equal(math.NewInt(1950000000)) { // manually checked amount
-			// last_block_height":"1062219"
-			if ctx.BlockHeight() == 1062219+1 { // todo: figure out height
+			bondedtotal := math.ZeroInt()
+			notbondedtotal := math.ZeroInt()
+			for _, v := range vals {
+				if v.IsBonded() {
+					bondedtotal = bondedtotal.Add(v.Tokens)
+				} else {
+					notbondedtotal = notbondedtotal.Add(v.Tokens)
+				}
+			}
+			// transfer amount should be 1950000000
+			// "spendable balance 97171770000loya is smaller than 99121770000loya
+			transferAmt := bondedPoolBal.Amount.Sub(bondedtotal)
+			if transferAmt.Equal(math.NewInt(1950000000)) { // manually checked amount
+				// last_block_height":"1062219"
+
 				err = app.BankKeeper.SendCoinsFromModuleToModule(ctx, stakingtypes.BondedPoolName, stakingtypes.NotBondedPoolName, sdk.NewCoins(sdk.NewCoin("loya", transferAmt)))
 				if err != nil {
 					return nil, err
