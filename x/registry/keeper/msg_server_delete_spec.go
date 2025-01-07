03c65d32ba1e493ba83c38d60db9b3498b4a1cf9
Cjpotter10 <caleb10joseph@gmail.com>
2025-01-07 11:18:09 -0600

    updated proto file but then ran into errors when trying to generate. Switching back to main to see if this is a branch problem or an overall problem

---
+++ x/registry/keeper/msg_server_delete_spec.go
@@ -0,0 +1,15 @@
+package keeper
+
+// import (
+// 	"context"
+// 	"fmt"
+// 	"strings"
+
+// 	"github.com/tellor-io/layer/x/registry/types"
+// 	"google.golang.org/grpc/codes"
+// 	"google.golang.org/grpc/status"
+
+// 	sdk "github.com/cosmos/cosmos-sdk/types"
+// )
+
+// func (k msgServer) RemoveDataSpec(goCtx context.Context, msg *types.)
