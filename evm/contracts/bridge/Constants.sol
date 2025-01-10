768a97790fd109307954146c6baee8dd58a068d9
tkernell <tpkernell@gmail.com>
2025-01-10 10:47:18 -0600

    R09 - use solidity 0.8.19

--- evm/contracts/bridge/Constants.sol
+++ evm/contracts/bridge/Constants.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.22;
+pragma solidity 0.8.19;
 
 /// @dev bytes32 encoding of the string "tellorCurrentAttestation"
 bytes32 constant NEW_REPORT_ATTESTATION_DOMAIN_SEPARATOR =
