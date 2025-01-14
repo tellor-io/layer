80153cb46c42d3693c143947fb70040b25d27f9b
danflo27 <florentinodc@outlook.com>
2025-01-14 08:43:24 -0500

    transfer old team vote if MsgUpdateTeam is called during a dispute

--- x/dispute/keeper/msg_update_team.go
+++ x/dispute/keeper/msg_update_team.go
@@ -3,9 +3,12 @@ package keeper
 import (
 	"bytes"
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/tellor-io/layer/x/dispute/types"
 
+	"cosmossdk.io/collections"
 	errorsmod "cosmossdk.io/errors"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -32,6 +35,50 @@ func (k msgServer) UpdateTeam(ctx context.Context, msg *types.MsgUpdateTeam) (*t
 		return nil, err
 	}
 	// if the team has voted on a dispute, transfer vote to the new address
-	
+	iter, err := k.Disputes.Indexes.OpenDisputes.MatchExact(ctx, true)
+	if err != nil {
+		return nil, err
+	}
+	defer iter.Close()
+	for ; iter.Valid(); iter.Next() {
+		key, err := iter.PrimaryKey()
+		if err != nil {
+			return nil, err
+		}
+		dispute, err := k.Disputes.Get(ctx, key)
+		if err != nil {
+			return nil, err
+		}
+		// if dispute is open, check if team has voted
+		if dispute.Open {
+			id := dispute.DisputeId
+			currentAccBytes := currentAcc.Bytes()
+			newAccBytes := newAcc.Bytes()
+			teamVoteExists, err := k.Voter.Has(ctx, collections.Join(id, currentAccBytes))
+			if err != nil {
+				if !errors.Is(err, collections.ErrNotFound) {
+					return nil, err
+				}
+			}
+			fmt.Println("teamVoteExists: ", teamVoteExists)
+			// if team has voted, remove previous team vote and set again with new address
+			if teamVoteExists {
+				vote, err := k.Voter.Get(ctx, collections.Join(id, currentAccBytes))
+				if err != nil {
+					return nil, err
+				}
+				fmt.Println("vote: ", vote)
+				err = k.Voter.Remove(ctx, collections.Join(id, currentAccBytes))
+				if err != nil {
+					return nil, err
+				}
+				err = k.Voter.Set(ctx, collections.Join(id, newAccBytes), vote)
+				if err != nil {
+					return nil, err
+				}
+			}
+		}
+	}
+
 	return &types.MsgUpdateTeamResponse{}, nil
 }
