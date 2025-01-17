1aad2ba723c8ae06aba1d3304268ebf5e5ab6bf4
Cjpotter10 <caleb10joseph@gmail.com>
2025-01-17 13:49:49 -0600

    fixed the issue with major disputes not jailing or deleting the reporter. We now will jail the reporter indefinitely but not deleting as the reporter can become unjailed upon winning the dispute

--- x/dispute/types/expected_keepers.go
+++ x/dispute/types/expected_keepers.go
@@ -52,4 +52,5 @@ type ReporterKeeper interface {
 	GetReporterTokensAtBlock(ctx context.Context, reporter []byte, blockNumber uint64) (math.Int, error)
 	GetDelegatorTokensAtBlock(ctx context.Context, delegator []byte, blockNumber uint64) (math.Int, error)
 	FeeRefund(ctx context.Context, hashId []byte, amt math.Int) error
+	UpdateJailedUntilOnFailedDispute(ctx context.Context, reporterAddr sdk.AccAddress) error
 }
