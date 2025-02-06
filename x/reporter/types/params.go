436859d48cd8f2f1e5b26ac6eee4012862b41841
Cjpotter10 <caleb10joseph@gmail.com>
2025-02-06 16:23:17 -0600

    Added max number of delegations as a param in the reporter module

--- x/reporter/types/params.go
+++ x/reporter/types/params.go
@@ -13,9 +13,13 @@ var _ paramtypes.ParamSet = (*Params)(nil)
 var (
 	KeyMinCommissionRate = []byte("MinCommissionRate")
 	// TODO: Determine the default value
-	DefaultMinCommissionRate = math.LegacyZeroDec()
-	DefaultMinLoya           = math.NewIntWithDecimal(1, 6)
-	DefaultMaxSelectors      = uint64(100)
+	DefaultMinCommissionRate   = math.LegacyZeroDec()
+	KeyMinLoya                 = []byte("MinLoya")
+	DefaultMinLoya             = math.NewIntWithDecimal(1, 6)
+	KeyMaxSelectors            = []byte("MaxSelectors")
+	DefaultMaxSelectors        = uint64(100)
+	KeyMaxNumOfDelegations     = []byte("MaxNumOfDelegations")
+	DefaultMaxNumOfDelegations = uint64(10)
 )
 
 // ParamKeyTable the param key table for launch module
@@ -27,11 +31,14 @@ func ParamKeyTable() paramtypes.KeyTable {
 func NewParams(
 	minCommissionRate math.LegacyDec,
 	minLoya math.Int,
+	maxSelectors uint64,
+	maxNumOfDelegations uint64,
 ) Params {
 	return Params{
-		MinCommissionRate: minCommissionRate,
-		MinLoya:           minLoya,
-		MaxSelectors:      DefaultMaxSelectors,
+		MinCommissionRate:   minCommissionRate,
+		MinLoya:             minLoya,
+		MaxSelectors:        maxSelectors,
+		MaxNumOfDelegations: maxNumOfDelegations,
 	}
 }
 
@@ -40,6 +47,8 @@ func DefaultParams() Params {
 	return NewParams(
 		DefaultMinCommissionRate,
 		DefaultMinLoya,
+		DefaultMaxSelectors,
+		DefaultMaxNumOfDelegations,
 	)
 }
 
@@ -47,12 +56,24 @@ func DefaultParams() Params {
 func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
 	return paramtypes.ParamSetPairs{
 		paramtypes.NewParamSetPair(KeyMinCommissionRate, &p.MinCommissionRate, validateMinCommissionRate),
+		paramtypes.NewParamSetPair(KeyMinLoya, &p.MinLoya, validateMinLoya),
+		paramtypes.NewParamSetPair(KeyMaxSelectors, &p.MaxSelectors, validateMaxSelectors),
+		paramtypes.NewParamSetPair(KeyMaxNumOfDelegations, &p.MaxNumOfDelegations, validateMaxNumOfDelegations),
 	}
 }
 
 // Validate validates the set of params
 func (p Params) Validate() error {
 	if err := validateMinCommissionRate(p.MinCommissionRate); err != nil {
+		return err
+	}
+	if err := validateMinLoya(p.MinLoya); err != nil {
+		return err
+	}
+	if err := validateMaxSelectors(p.MaxSelectors); err != nil {
+		return err
+	}
+	if err := validateMaxNumOfDelegations(p.MaxNumOfDelegations); err != nil {
 		return err
 	}
 
@@ -62,6 +83,33 @@ func (p Params) Validate() error {
 // validateMinStakeAmount validates the MinStakeAmount param
 func validateMinCommissionRate(v interface{}) error {
 	_, ok := v.(math.LegacyDec)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", v)
+	}
+
+	return nil
+}
+
+func validateMinLoya(v interface{}) error {
+	_, ok := v.(math.Int)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", v)
+	}
+
+	return nil
+}
+
+func validateMaxSelectors(v interface{}) error {
+	_, ok := v.(uint64)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", v)
+	}
+
+	return nil
+}
+
+func validateMaxNumOfDelegations(v interface{}) error {
+	_, ok := v.(uint64)
 	if !ok {
 		return fmt.Errorf("invalid parameter type: %T", v)
 	}
