48a8cd77904820895a0372668d7707a10780513c
danflo27 <florentinodc@outlook.com>
2025-02-11 12:41:38 -0500

    slashing interchaintest

---
+++ e2e/slash_test.go
@@ -0,0 +1,147 @@
+package e2e_test
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"testing"
+	"time"
+
+	interchaintest "github.com/strangelove-ventures/interchaintest/v8"
+	"github.com/strangelove-ventures/interchaintest/v8/chain/cosmos"
+	"github.com/strangelove-ventures/interchaintest/v8/ibc"
+	"github.com/stretchr/testify/require"
+	"github.com/tellor-io/layer/e2e"
+
+	"cosmossdk.io/math"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+)
+
+// cd e2e
+// go test -run TestInactivitySlash --timeout 5m
+
+// start with 4 validators, one of them goes offline
+func TestInactivitySlash(t *testing.T) {
+	require := require.New(t)
+
+	t.Helper()
+	if testing.Short() {
+		t.Skip("skipping in short mode")
+	}
+
+	t.Parallel()
+	cosmos.SetSDKConfig("tellor")
+
+	modifyGenesis := []cosmos.GenesisKV{
+		cosmos.NewGenesisKV("app_state.dispute.params.team_address", sdk.MustAccAddressFromBech32("tellor14ncp4jg0d087l54pwnp8p036s0dc580xy4gavf").Bytes()),
+		cosmos.NewGenesisKV("consensus.params.abci.vote_extensions_enable_height", "1"),
+		cosmos.NewGenesisKV("app_state.gov.params.voting_period", "15s"),
+		cosmos.NewGenesisKV("app_state.gov.params.max_deposit_period", "10s"),
+		cosmos.NewGenesisKV("app_state.gov.params.min_deposit.0.denom", "loya"),
+		cosmos.NewGenesisKV("app_state.gov.params.min_deposit.0.amount", "1"),
+		cosmos.NewGenesisKV("app_state.globalfee.params.minimum_gas_prices.0.amount", "0.0"),
+		cosmos.NewGenesisKV("app_state.slashing.params.signed_blocks_window", "2"),
+	}
+
+	nv := 4
+	nf := 0
+	chains := interchaintest.CreateChainsWithChainSpecs(t, []*interchaintest.ChainSpec{
+		{
+			NumValidators: &nv,
+			NumFullNodes:  &nf,
+			ChainConfig: ibc.ChainConfig{
+				Type:           "cosmos",
+				Name:           "layer",
+				ChainID:        "layer",
+				Bin:            "layerd",
+				Denom:          "loya",
+				Bech32Prefix:   "tellor",
+				CoinType:       "118",
+				GasPrices:      "0.0loya",
+				GasAdjustment:  1.1,
+				TrustingPeriod: "504h",
+				NoHostMount:    false,
+				Images: []ibc.DockerImage{
+					{
+						Repository: "layer",
+						Version:    "local",
+						UidGid:     "1025:1025",
+					},
+				},
+				EncodingConfig:      e2e.LayerEncoding(),
+				ModifyGenesis:       cosmos.ModifyGenesis(modifyGenesis),
+				AdditionalStartArgs: []string{"--key-name", "validator", "--price-daemon-enabled=false"},
+			},
+		},
+	})
+
+	client, network := interchaintest.DockerSetup(t)
+
+	chain := chains[0].(*cosmos.CosmosChain)
+
+	ic := interchaintest.NewInterchain().
+		AddChain(chain)
+
+	ctx := context.Background()
+
+	require.NoError(ic.Build(ctx, nil, interchaintest.InterchainBuildOptions{
+		TestName:  t.Name(),
+		Client:    client,
+		NetworkID: network,
+		// BlockDatabaseFile: interchaintest.DefaultBlockDatabaseFilepath(),
+		SkipPathCreation: false,
+	}))
+	t.Cleanup(func() {
+		_ = ic.Close()
+	})
+	teamMnemonic := "unit curious maid primary holiday lunch lift melody boil blossom three boat work deliver alpha intact tornado october process dignity gravity giggle enrich output"
+	require.NoError(chain.RecoverKey(ctx, "team", teamMnemonic))
+	require.NoError(chain.SendFunds(ctx, "faucet", ibc.WalletAmount{
+		Address: "tellor14ncp4jg0d087l54pwnp8p036s0dc580xy4gavf",
+		Amount:  math.NewInt(1000000000000),
+		Denom:   "loya",
+	}))
+
+	val1 := chain.Validators[0]
+	val1Addr, err := val1.AccountKeyBech32(ctx, "validator")
+	require.NoError(err)
+	val1valAddr, err := val1.KeyBech32(ctx, "validator", "val")
+	require.NoError(err)
+	fmt.Println("val1 Account Address: ", val1Addr)
+	fmt.Println("val1 Validator Address: ", val1valAddr)
+	val2 := chain.Validators[1]
+	val2Addr, err := val2.AccountKeyBech32(ctx, "validator")
+	require.NoError(err)
+	val2valAddr, err := val2.KeyBech32(ctx, "validator", "val")
+	require.NoError(err)
+	fmt.Println("val2 Account Address: ", val2Addr)
+	fmt.Println("val2 Validator Address: ", val2valAddr)
+
+	// queryValidators to confirm that 4 validators are bonded
+	vals, err := chain.StakingQueryValidators(ctx, stakingtypes.BondStatusBonded)
+	require.NoError(err)
+	require.Equal(len(vals), 4)
+
+	// val 1 goes offline
+	err = val1.PauseContainer(ctx)
+	fmt.Println("paused val1")
+	require.NoError(err)
+
+	time.Sleep(10 * time.Second)
+
+	// only 3 validators should be bonded now
+	fmt.Println("querying validators...")
+	valsQueryRes, _, err := val2.ExecQuery(ctx, "staking", "validators")
+	require.NoError(err)
+	var validatorsRes e2e.QueryValidatorsResponse
+	err = json.Unmarshal(valsQueryRes, &validatorsRes)
+	require.NoError(err)
+	fmt.Println("validatorsRes: ", validatorsRes)
+	require.Equal(len(validatorsRes.Validators), 4)
+	require.Equal(validatorsRes.Validators[0].Jailed, true)
+	require.Equal(validatorsRes.Validators[1].Jailed, false)
+	require.Equal(validatorsRes.Validators[2].Jailed, false)
+	require.Equal(validatorsRes.Validators[3].Jailed, false)
+}
