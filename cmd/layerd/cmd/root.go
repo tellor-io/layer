ffe948c12bf9c80c8f0295454700ed0af21296b7
Cjpotter10 <caleb10joseph@gmail.com>
2025-03-25 14:26:35 -0500

    have collect-gentxs-delegate tested and working. Added USDT-USD pair and USDC-USD pair to the code so that all nodes automatically support them when they init a new node. Updated some scripts with small changes

--- cmd/layerd/cmd/root.go
+++ cmd/layerd/cmd/root.go
@@ -156,23 +156,13 @@ func initRootCmd(
 
 // genesisCommand builds genesis-related `simd genesis` command. Users may provide application specific commands as a parameter
 func genesisCommand(txConfig client.TxConfig, basicManager module.BasicManager, cmds ...*cobra.Command) *cobra.Command {
-	cmd := &cobra.Command{
-		Use:                        "genesis",
-		Short:                      "Application's genesis-related subcommands",
-		DisableFlagParsing:         false,
-		SuggestionsMinimumDistance: 2,
-		RunE:                       client.ValidateCmd,
+	cmd := genutilcli.Commands(txConfig, basicManager, app.DefaultNodeHome)
+
+	for _, subCmd := range cmds {
+		cmd.AddCommand(subCmd)
 	}
-
-	cmd.AddCommand(
-		genutilcli.GenTxCmd(basicManager, txConfig, banktypes.GenesisBalancesIterator{}, app.DefaultNodeHome, txConfig.SigningContext().ValidatorAddressCodec()),
-		genutilcli.MigrateGenesisCmd(MigrationMap),
-		genutilcli.CollectGenTxsCmd(banktypes.GenesisBalancesIterator{}, app.DefaultNodeHome, app.CustomMessageValidator, txConfig.SigningContext().ValidatorAddressCodec()),
-		genutilcli.ValidateGenesisCmd(basicManager),
-		genutilcli.AddGenesisAccountCmd(app.DefaultNodeHome, txConfig.SigningContext().AddressCodec()),
-		AddTeamAccountCmd(app.DefaultNodeHome),
-	)
-
+	cmd.AddCommand(AddTeamAccountCmd(app.DefaultNodeHome))
+	cmd.AddCommand(CollectGenTxsWithDelegateCmd(banktypes.GenesisBalancesIterator{}, app.DefaultNodeHome, app.CustomMessageValidator, txConfig.SigningContext().ValidatorAddressCodec()))
 	return cmd
 }
 
