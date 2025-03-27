dbdd6a02b72832c1f3ee4ddd7c4f1069ade2469f
tkernell <tpkernell@gmail.com>
2025-03-27 10:20:13 -0500

    v4.0.1 upgrade handler (#641)
    Co-authored-by: themandalore <fett.nicholas@yahoo.com>

---
+++ app/upgrades/v4.0.1/upgrade.go
@@ -0,0 +1,34 @@
+package v_3_0_4
+
+import (
+	"context"
+	"fmt"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+)
+
+/*
+	Upgrade to v4.0.1 will include:
+		- stop node if keys not accessible for signing attestations
+		- added genesis command collect-gentxs-delegate
+		- updated autocli/proto files
+		- updated scripts
+
+	Migrations:
+		- None
+*/
+
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+) upgradetypes.UpgradeHandler {
+	return func(ctx context.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		sdkCtx := sdk.UnwrapSDKContext(ctx)
+		sdkCtx.Logger().Info(fmt.Sprintf("Running %s Upgrade...", UpgradeName))
+
+		return mm.RunMigrations(ctx, configurator, vm)
+	}
+}
