c4e1ff1eb0c2b2203f29e9e3b1a9627e9955ff7d
tkernell <tpkernell@gmail.com>
2025-04-07 10:58:15 -0500

    check withdraw addr len and test

--- x/bridge/keeper/msg_server_withdraw_tokens_test.go
+++ x/bridge/keeper/msg_server_withdraw_tokens_test.go
@@ -1,6 +1,7 @@
 package keeper_test
 
 import (
+	"encoding/hex"
 	"testing"
 
 	"github.com/cometbft/cometbft/crypto/secp256k1"
@@ -75,6 +76,56 @@ func TestMsgWithdrawTokens(t *testing.T) {
 	response, err = msgServer.WithdrawTokens(ctx, &types.MsgWithdrawTokens{
 		Creator:   creatorAddr.String(),
 		Recipient: "3536373839306162636465663132333435363738",
+		Amount:    amount,
+	})
+	require.NoError(t, err)
+	require.NotNil(t, response)
+}
+
+func TestMsgWithdrawTokensBadRecipient(t *testing.T) {
+	k, _, bk, ok, _, sk, _, ctx := setupKeeper(t)
+	require.NotNil(t, k)
+	require.NotNil(t, ctx)
+	msgServer := keeper.NewMsgServerImpl(k)
+
+	creatorAddr := sdk.AccAddress(secp256k1.GenPrivKey().PubKey().Address())
+	badRecipientInvalidHex := "1234567890zbcdef1234567890abcdef12345678"
+	badRecipientInvalidLength := "1234567890abcdef1234567890abcdef123456"
+	goodRecipientAddr := "1234567890abcdef1234567890abcdef12345678"
+
+	// bad recipient invalid hex
+	response, err := msgServer.WithdrawTokens(ctx, &types.MsgWithdrawTokens{
+		Creator:   creatorAddr.String(),
+		Recipient: badRecipientInvalidHex,
+		Amount:    sdk.Coin{Denom: "loya", Amount: math.NewInt(10 * 1e6)},
+	})
+	require.ErrorContains(t, err, "invalid request")
+	require.Nil(t, response)
+
+	// bad recipient invalid length
+	response, err = msgServer.WithdrawTokens(ctx, &types.MsgWithdrawTokens{
+		Creator:   creatorAddr.String(),
+		Recipient: badRecipientInvalidLength,
+		Amount:    sdk.Coin{Denom: "loya", Amount: math.NewInt(10 * 1e6)},
+	})
+	require.ErrorContains(t, err, "invalid request")
+	require.Nil(t, response)
+
+	// good recipient
+	amount := sdk.Coin{Denom: "loya", Amount: math.NewInt(10 * 1e6)}
+	bk.On("SendCoinsFromAccountToModule", ctx, creatorAddr, types.ModuleName, sdk.NewCoins(amount)).Return(nil)
+	bk.On("BurnCoins", ctx, types.ModuleName, sdk.NewCoins(amount)).Return(nil)
+	sk.On("TotalBondedTokens", ctx).Return(math.NewInt(10*1e6), nil)
+	goodRecipientHex, err := hex.DecodeString(goodRecipientAddr)
+	require.NoError(t, err)
+	agg, queryData, err := k.CreateWithdrawalAggregate(ctx, amount, creatorAddr, goodRecipientHex, 1)
+	require.NoError(t, err)
+	require.NotNil(t, agg)
+	require.NotNil(t, queryData)
+	ok.On("SetAggregate", ctx, agg, queryData).Return(nil)
+	response, err = msgServer.WithdrawTokens(ctx, &types.MsgWithdrawTokens{
+		Creator:   creatorAddr.String(),
+		Recipient: goodRecipientAddr,
 		Amount:    amount,
 	})
 	require.NoError(t, err)
