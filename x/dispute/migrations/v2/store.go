e5b8cd4d0d55c8a781f65aae8049dc3b4fe9d792
Cjpotter10 <caleb10joseph@gmail.com>
2025-04-28 14:43:02 -0500

    made dispute migrations function take in the dispute indexed map and moved the indexes.go file from the keeper to the types as well as the tests

--- x/dispute/migrations/v2/store.go
+++ x/dispute/migrations/v2/store.go
@@ -2,42 +2,23 @@ package v2
 
 import (
 	"context"
-	"fmt"
-
-	disputetypes "github.com/tellor-io/layer/x/dispute/types"
 
 	"cosmossdk.io/collections"
-	"cosmossdk.io/core/store"
-	"cosmossdk.io/store/prefix"
-
-	"github.com/cosmos/cosmos-sdk/codec"
-	"github.com/cosmos/cosmos-sdk/runtime"
+	"github.com/tellor-io/layer/x/dispute/types"
 )
 
-func MigrateStore(ctx context.Context, storeService store.KVStoreService, cdc codec.BinaryCodec) error {
-	store := runtime.KVStoreAdapter(storeService.OpenKVStore(ctx))
-	disputeStore := prefix.NewStore(store, disputetypes.DisputesPrefix)
-
+func MigrateStore(ctx context.Context, disputesMap *collections.IndexedMap[uint64, types.Dispute, types.DisputesIndex]) error {
 	// The keys are just the uint64 dispute IDs encoded
 	for id := uint64(2); id <= 3; id++ {
-		key := make([]byte, 8)
-		_, err := collections.Uint64Key.Encode(key, id)
+		dispute, err := disputesMap.Get(ctx, id)
 		if err != nil {
 			return err
 		}
-		if disputeBytes := disputeStore.Get(key); disputeBytes != nil {
-			var dispute disputetypes.Dispute
-			if err := cdc.Unmarshal(disputeBytes, &dispute); err != nil {
-				return err
-			}
-			dispute.Open = false
-			newData, err := cdc.Marshal(&dispute)
-			if err != nil {
-				return err
-			}
-			disputeStore.Set(key, newData)
-		} else {
-			return fmt.Errorf("dispute %d not found", id)
+
+		dispute.Open = false
+		err = disputesMap.Set(ctx, id, dispute)
+		if err != nil {
+			return err
 		}
 	}
 
