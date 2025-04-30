0050f8f08646919cd7e93d0bd7ece055d5fa860d
Cjpotter10 <caleb10joseph@gmail.com>
2025-04-30 10:58:35 -0500

    got branch ready to upgrade using the fork migration scripts to read in exported module data that was not mission critical enough to be included in the genesis file for readability and memory management purposes

---
+++ app/upgrades/fork/upgrade.go
@@ -0,0 +1,33 @@
+package fork
+
+import (
+	"context"
+	"fmt"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+)
+
+/*
+	Upgrade to fork will include:
+		- reading in module state data from json files that were created and exported from the previous version
+
+	Migrations:
+		- dispute
+		- reporter
+		- oracle
+*/
+
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+) upgradetypes.UpgradeHandler {
+	return func(ctx context.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		sdkCtx := sdk.UnwrapSDKContext(ctx)
+		sdkCtx.Logger().Info(fmt.Sprintf("Running %s Upgrade...", UpgradeName))
+
+		return mm.RunMigrations(ctx, configurator, vm)
+	}
+}
