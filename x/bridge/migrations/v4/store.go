6752ea224cc7eb68c1d0577076e134b369b4c814
tkernell <tpkernell@gmail.com>
2025-06-27 12:38:38 -0500

    migrate EVMToOperatorAddressMap for existing validators

--- x/bridge/migrations/v4/store.go
+++ x/bridge/migrations/v4/store.go
@@ -2,15 +2,18 @@ package v4
 
 import (
 	"context"
+	"encoding/hex"
 
 	"github.com/gogo/protobuf/proto"
 	bridgetypes "github.com/tellor-io/layer/x/bridge/types"
 
 	"cosmossdk.io/core/store"
 	"cosmossdk.io/store/prefix"
+	storetypes "cosmossdk.io/store/types"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/runtime"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // ValidatorCheckpointParamsLegacy represents the old ValidatorCheckpointParams struct without BlockHeight
@@ -29,12 +32,29 @@ func (m *ValidatorCheckpointParamsLegacy) String() string {
 }
 func (*ValidatorCheckpointParamsLegacy) ProtoMessage() {}
 
-// MigrateStore migrates the ValidatorCheckpointParams from the legacy format to the new format
-// by adding the BlockHeight field with a default value of 0
+// MigrateStore migrates the bridge module from v3 to v4:
+// 1. Migrates ValidatorCheckpointParams from the legacy format to the new format by adding the BlockHeight field
+// 2. Populates EVMToOperatorAddressMap from existing OperatorToEVMAddressMap entries for slashing functionality
 func MigrateStore(ctx context.Context, storeService store.KVStoreService, cdc codec.BinaryCodec) error {
 	store := runtime.KVStoreAdapter(storeService.OpenKVStore(ctx))
 
-	// Migrate ValidatorCheckpointParams
+	// migrate ValidatorCheckpointParams (existing migration)
+	err := migrateValidatorCheckpointParams(store, cdc)
+	if err != nil {
+		return err
+	}
+
+	// migrate EVMToOperatorAddressMap (new migration)
+	err = migrateEVMToOperatorAddressMap(store, cdc)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// migrateValidatorCheckpointParams migrates ValidatorCheckpointParams from legacy format to new format
+func migrateValidatorCheckpointParams(store storetypes.KVStore, cdc codec.BinaryCodec) error {
 	checkpointStore := prefix.NewStore(store, bridgetypes.ValidatorCheckpointParamsMapKey)
 	iter := checkpointStore.Iterator(nil, nil)
 	defer iter.Close()
@@ -60,6 +80,59 @@ func MigrateStore(ctx context.Context, storeService store.KVStoreService, cdc co
 		}
 
 		checkpointStore.Set(iter.Key(), newData)
+	}
+
+	return nil
+}
+
+// migrateEVMToOperatorAddressMap populates EVMToOperatorAddressMap from existing OperatorToEVMAddressMap entries
+func migrateEVMToOperatorAddressMap(store storetypes.KVStore, cdc codec.BinaryCodec) error {
+	// get the operator to EVM address map store
+	operatorToEVMStore := prefix.NewStore(store, bridgetypes.OperatorToEVMAddressMapKey)
+	operatorIter := operatorToEVMStore.Iterator(nil, nil)
+	defer operatorIter.Close()
+
+	// get the EVM to operator address map store for writing
+	evmToOperatorStore := prefix.NewStore(store, bridgetypes.EVMToOperatorAddressMapKey)
+
+	for ; operatorIter.Valid(); operatorIter.Next() {
+		// decode the operator address from the key
+		operatorAddr := string(operatorIter.Key())
+
+		// decode the EVM address from the value
+		var evmAddr bridgetypes.EVMAddress
+		if err := cdc.Unmarshal(operatorIter.Value(), &evmAddr); err != nil {
+			// log error but continue migration for other entries
+			continue
+		}
+
+		// convert EVM address bytes to hex string (this is how it's stored as key in EVMToOperatorAddressMap)
+		evmAddressString := hex.EncodeToString(evmAddr.EVMAddress)
+
+		// check if reverse mapping already exists
+		evmKey := []byte(evmAddressString)
+		if evmToOperatorStore.Has(evmKey) {
+			// mapping already exists, skip
+			continue
+		}
+
+		// create the operator address type
+		sdkValAddr, err := sdk.ValAddressFromBech32(operatorAddr)
+		if err != nil {
+			continue
+		}
+
+		operatorAddrType := bridgetypes.OperatorAddress{
+			OperatorAddress: sdkValAddr,
+		}
+
+		operatorData, err := cdc.Marshal(&operatorAddrType)
+		if err != nil {
+			continue
+		}
+
+		// set the reverse mapping
+		evmToOperatorStore.Set(evmKey, operatorData)
 	}
 
 	return nil
