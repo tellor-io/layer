0b066a708e00a6cddbea507cc0ae083469b92710
danflo27 <florentinodc@outlook.com>
2025-07-21 22:23:33 -0400

    get tip total unit test and check for nil request

--- x/oracle/keeper/query_get_user_tip_total.go
+++ x/oracle/keeper/query_get_user_tip_total.go
@@ -25,6 +25,10 @@ func (q Querier) GetUserTipTotal(ctx context.Context, req *types.QueryGetUserTip
 }
 
 func (q Querier) GetTipTotal(ctx context.Context, req *types.QueryGetTipTotalRequest) (*types.QueryGetTipTotalResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
 	totalTips, err := q.keeper.GetTotalTips(ctx)
 	if err != nil {
 		return nil, err
