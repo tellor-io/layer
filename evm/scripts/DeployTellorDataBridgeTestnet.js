740ffef75d12bd8a172bbeff136bbcf62244705e
tkernell <tpkernell@gmail.com>
2025-09-26 08:21:36 -0500

    Sepolia forking (#827)
    * tokenbridge init function for testnet
    
    * more sanity checks in sepolia upgrade script
    
    * forking tests
    
    ---------
    
    Co-authored-by: themandalore <fett.nicholas@yahoo.com>
    (cherry picked from commit add072f1b9494711c7fd29d8d18a07fdf76dc477)

---
+++ evm/scripts/DeployTellorDataBridgeTestnet.js
@@ -0,0 +1,42 @@
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+const h = require("../test/helpers/evmHelpers");
+
+// npx hardhat run scripts/DeployTellorDataBridgeTestnet.js --network sepolia
+
+// update these variables
+var guardianaddress = " "
+var tellorChainId = " "
+var PK = process.env.TESTNET_PK
+var NODE_URL = process.env.NODE_URL_SEPOLIA_TESTNET
+
+valsetDomainSep = h.getDomainSeparator(tellorChainId)
+
+async function deployTellorDataBridge(_pk, _nodeURL) {
+    console.log("deploy TellorDataBridgeTestnet")
+    console.log("guardianaddress", guardianaddress)
+    console.log("tellorChainId", tellorChainId)
+    console.log("valsetDomainSep", valsetDomainSep)
+    // var net = hre.network.name
+    await run("compile")
+
+    //Connect to the network
+    let privateKey = _pk;
+    var provider = new ethers.providers.JsonRpcProvider(_nodeURL)
+    let wallet = new ethers.Wallet(privateKey, provider);
+    
+    ////////  Deploy TellorDataBridge contract  ////////////////////////
+    console.log("deploy TellorDataBridge")
+    const TellorDataBridge = await ethers.getContractFactory("contracts/testing/TellorDataBridgeTestnet.sol:TellorDataBridgeTestnet", wallet);
+    const tellorDataBridge= await TellorDataBridge.deploy(guardianaddress, valsetDomainSep);
+    await tellorDataBridge.deployed();
+    console.log("TellorDataBridge deployed to:", tellorDataBridge.address);
+  };
+
+  deployTellorDataBridge(PK, NODE_URL)
+    .then(() => process.exit(0))
+    .catch(error => {
+	  console.error(error);
+	  process.exit(1);
+  });
