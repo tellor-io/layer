f3dea586131538729a9fa55c462a44f07aa7bbd2
danflo27 <florentinodc@outlook.com>
2025-10-07 13:18:28 -0400

    fix gas test tip timing

--- e2e/gas_test.go
+++ e2e/gas_test.go
@@ -3,6 +3,7 @@ package e2e_test
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/strangelove-ventures/interchaintest/v8/chain/cosmos"
 	"github.com/strangelove-ventures/interchaintest/v8/testutil"
@@ -17,22 +18,16 @@ func TestGas(t *testing.T) {
 
 	cosmos.SetSDKConfig("tellor")
 
-	chain, ic, ctx := e2e.SetupChain(t, 4, 0)
+	chain, ic, ctx := e2e.SetupChain(t, 2, 0)
 	defer ic.Close()
 
 	val1 := chain.Validators[0]
 	val2 := chain.Validators[1]
-	val3 := chain.Validators[2]
-	val4 := chain.Validators[3]
 
-	// all 4 vals become reporters
+	// all 2 vals become reporters
 	_, err := chain.GetNode().ExecTx(ctx, "validator", "reporter", "create-reporter", math.NewUint(0).String(), math.NewUint(1_000_000).String(), "val1_moniker", "--keyring-dir", val1.HomeDir())
 	require.NoError(err)
 	_, err = val2.ExecTx(ctx, "validator", "reporter", "create-reporter", math.NewUint(0).String(), math.NewUint(1_000_000).String(), "val2_moniker", "--keyring-dir", val2.HomeDir())
-	require.NoError(err)
-	_, err = val3.ExecTx(ctx, "validator", "reporter", "create-reporter", math.NewUint(0).String(), math.NewUint(1_000_000).String(), "val3_moniker", "--keyring-dir", val3.HomeDir())
-	require.NoError(err)
-	_, err = val4.ExecTx(ctx, "validator", "reporter", "create-reporter", math.NewUint(0).String(), math.NewUint(1_000_000).String(), "val4_moniker", "--keyring-dir", val4.HomeDir())
 	require.NoError(err)
 
 	// wait 1 block
@@ -41,6 +36,14 @@ func TestGas(t *testing.T) {
 	// tip query
 	_, _, err = val1.Exec(ctx, val1.TxCommand("validator", "oracle", "tip", qData, "1000000loya", "--keyring-dir", chain.HomeDir()), chain.Config().Env)
 	require.NoError(err)
+
+	timeNow := time.Now()
+	fmt.Println("time1: ", timeNow)
+	// wait 1 block
+	require.NoError(testutil.WaitForBlocks(ctx, 1, val1))
+	timeNow2 := time.Now()
+	fmt.Println("time2: ", timeNow2)
+	fmt.Println("time2 - time1: ", timeNow2.Sub(timeNow))
 
 	t.Run("val1", func(t *testing.T) {
 		t.Parallel()
