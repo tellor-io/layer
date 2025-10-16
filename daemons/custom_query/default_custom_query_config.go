922caa35028e3533955391321dcb269fe2468d52
akrem <71235284+akremstudy@users.noreply.github.com>
2025-10-16 12:46:26 -0400

    Add yieldFi-yeth feed (#843)

--- daemons/custom_query/default_custom_query_config.go
+++ daemons/custom_query/default_custom_query_config.go
@@ -46,7 +46,7 @@ const (
     [queries.{{ $key }}]
     id = "{{ $query.ID }}"
     aggregation_method = "{{ $query.AggregationMethod }}"
-	max_spread_percent = {{ $query.MaxSpreadPercent }}
+	max_spread_percent = {{ tomlValue $query.MaxSpreadPercent }}
     min_responses = {{ $query.MinResponses }}
     response_type = "{{ $query.ResponseType }}"
 
