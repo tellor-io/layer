44fad3d7fe5a62129ce62d66675e1191521c9f6a
Cjpotter10 <caleb10joseph@gmail.com>
2025-10-20 10:53:34 -0500

    tried to pull in and rebase from main. and simplified the upgrade handler

--- app/upgrades/v6.0.0/upgrade.go
+++ app/upgrades/v6.0.0/upgrade.go
@@ -4,14 +4,11 @@ import (
 	"context"
 	"fmt"
 
-	minttypes "github.com/tellor-io/layer/x/mint/types"
-
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/cosmos/cosmos-sdk/x/auth/keeper"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 )
 
 /*
@@ -29,17 +26,7 @@ func CreateUpgradeHandler(
 	return func(ctx context.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		sdkCtx := sdk.UnwrapSDKContext(ctx)
 		sdkCtx.Logger().Info(fmt.Sprintf("Running %s Upgrade...", UpgradeName))
-		if acc := ak.GetModuleAccount(sdkCtx, minttypes.ExtraRewardsPool); acc == nil {
-			sdkCtx.Logger().Info("Creating ExtraRewardsPool module account")
 
-			addr := authtypes.NewModuleAddress(minttypes.ExtraRewardsPool)
-			extraRewardsPoolMacc := authtypes.NewEmptyModuleAccount(minttypes.ExtraRewardsPool)
-			ak.SetModuleAccount(sdkCtx, extraRewardsPoolMacc)
-
-			sdkCtx.Logger().Info("Created ExtraRewardsPool module account", "address", addr.String())
-		} else {
-			sdkCtx.Logger().Info("ExtraRewardsPool module account already exists")
-		}
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
 }
