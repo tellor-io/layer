372bc31cc4cbeec25786eec60ad90142b6731924
akrem <71235284+akremstudy@users.noreply.github.com>
2026-02-05 13:25:30 -0500

    make bridge address based on chain id and add .env support

--- app/upgrades/v6.1.2/upgrade.go
+++ app/upgrades/v6.1.2/upgrade.go
@@ -3,6 +3,7 @@ package v6_1_2
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/ethereum/go-ethereum/common"
 	bridgekeeper "github.com/tellor-io/layer/x/bridge/keeper"
@@ -21,11 +22,16 @@ import (
 
 /*
 Upgrade to v6.1.2 includes:
-	- Deprecation of TRBBridge query type in favor of TRBBridgeV2
-	- Updates to oracle keeper to handle TRBBridgeV2 query type and prevent usage of TRBBridge
+  - Deprecation of TRBBridge query type in favor of TRBBridgeV2
+  - Updates to oracle keeper to handle TRBBridgeV2 query type and prevent usage of TRBBridge
 */
-// TODO: set to deployed TokenBridgeV2 contract address before building upgrade binary
-const TokenBridgeV2Address = "0x0000000000000000000000000000000000000000"
+// TODO: change these to real addresses
+const (
+	MainnetChainID       = "tellor1"
+	MainnetTokenBridgeV2 = "0x0000000000000000000000000000000000000000"
+	TestnetChainID       = "layertest-4"
+	TestnetTokenBridgeV2 = "0x0000000000000000000000000000000000000000"
+)
 
 func CreateUpgradeHandler(
 	mm *module.Manager,
@@ -61,8 +67,14 @@ func CreateUpgradeHandler(
 		if err != nil {
 			return nil, err
 		}
+
+		tokenBridgeV2Address, err := resolveTokenBridgeV2Address(sdkCtx.ChainID())
+		if err != nil {
+			return nil, err
+		}
+
 		withdrawalId := uint64(1_000_000_000_000)
-		recipient := common.HexToAddress(TokenBridgeV2Address).Bytes()
+		recipient := common.HexToAddress(tokenBridgeV2Address).Bytes()
 		sender := authtypes.NewModuleAddress(bridgetypes.ModuleName)
 		amount := sdk.NewCoin("loya", math.NewInt(2_800_000_000_000)) // 2.8 million TRB - 1 TRB = 1_000_000 loya
 
@@ -76,5 +88,20 @@ func CreateUpgradeHandler(
 		}
 		sdkCtx.Logger().Info(fmt.Sprintf("Running %s Upgrade...", UpgradeName))
 		return mm.RunMigrations(ctx, configurator, vm)
+	}
+}
+
+func resolveTokenBridgeV2Address(chainID string) (string, error) {
+	switch chainID {
+	case MainnetChainID:
+		return MainnetTokenBridgeV2, nil
+	case TestnetChainID:
+		return TestnetTokenBridgeV2, nil
+	default:
+		addr := os.Getenv("TOKEN_BRIDGE_V2_ADDRESS")
+		if addr == "" {
+			return "", fmt.Errorf("TOKEN_BRIDGE_V2_ADDRESS env var not set for chain-id %s", chainID)
+		}
+		return addr, nil
 	}
 }
