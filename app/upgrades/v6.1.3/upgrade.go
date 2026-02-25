4e6709c9c3c80da29e20a4fa6f23e2abd6addf03
CJPotter10 <91627020+CJPotter10@users.noreply.github.com>
2026-02-25 13:42:12 -0600

    Upgrade handler v6.1.3 (#964)
    * added upgrade handler for v6.1.3
    
    * updated upgrade name in test

---
+++ app/upgrades/v6.1.3/upgrade.go
@@ -0,0 +1,29 @@
+package v6_1_3
+
+import (
+	"context"
+	"fmt"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+)
+
+/*
+Upgrade to v6.1.3 includes:
+  - extra checks added to the vote extension handler to prevent abuse
+  - removed some panics and added further fuzz testing
+*/
+
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+) upgradetypes.UpgradeHandler {
+	return func(ctx context.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		sdkCtx := sdk.UnwrapSDKContext(ctx)
+		sdkCtx.Logger().Info(fmt.Sprintf("Running %s Upgrade...", UpgradeName))
+
+		return mm.RunMigrations(ctx, configurator, vm)
+	}
+}
