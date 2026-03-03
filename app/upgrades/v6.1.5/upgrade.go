ef69143be1609d6124ed9f681a8f430862d7de82
Cjpotter10 <caleb10joseph@gmail.com>
2026-03-03 10:52:42 -0600

    added upgrade handler for bridge transition

---
+++ app/upgrades/v6.1.5/upgrade.go
@@ -0,0 +1,28 @@
+package v6_1_5
+
+import (
+	"context"
+	"fmt"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+)
+
+/*
+Upgrade to v6.1.5 includes:
+  - added a new bridge deposit query type to support the v2 token bridge
+*/
+
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+) upgradetypes.UpgradeHandler {
+	return func(ctx context.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		sdkCtx := sdk.UnwrapSDKContext(ctx)
+		sdkCtx.Logger().Info(fmt.Sprintf("Running %s Upgrade...", UpgradeName))
+
+		return mm.RunMigrations(ctx, configurator, vm)
+	}
+}
