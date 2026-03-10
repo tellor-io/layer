db1bb14be94fb246f7c4afe1a21d55339cfa09d6
tkernell <tpkernell@gmail.com>
2026-03-10 11:05:16 -0500

    upgrades

--- app/upgrades.go
+++ app/upgrades.go
@@ -4,11 +4,7 @@ import (
 	"fmt"
 
 	"github.com/tellor-io/layer/app/upgrades"
-<<<<<<< HEAD
 	v_6_1_4 "github.com/tellor-io/layer/app/upgrades/v6.1.4"
-=======
-	v_6_1_3 "github.com/tellor-io/layer/app/upgrades/v6.1.3"
->>>>>>> 2d1e37f962adb56bca35f67313a988c6d6611fd5
 
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 )
@@ -17,11 +13,7 @@ var (
 	// `Upgrades` defines the upgrade handlers and store loaders for the application.
 	// New upgrades should be added to this slice after they are implemented.
 	Upgrades = []*upgrades.Upgrade{
-<<<<<<< HEAD
 		&v_6_1_4.Upgrade,
-=======
-		&v_6_1_3.Upgrade,
->>>>>>> 2d1e37f962adb56bca35f67313a988c6d6611fd5
 	}
 	Forks = []upgrades.Fork{}
 )
@@ -29,21 +21,12 @@ var (
 // setupUpgradeHandlers registers the upgrade handlers to perform custom upgrade
 // logic and state migrations for software upgrades.
 func (app *App) setupUpgradeHandlers() {
-<<<<<<< HEAD
 	if app.UpgradeKeeper.HasHandler(v_6_1_4.UpgradeName) {
 		panic(fmt.Sprintf("Cannot register duplicate upgrade handler '%s'", v_6_1_4.UpgradeName))
 	}
 	app.UpgradeKeeper.SetUpgradeHandler(
 		v_6_1_4.UpgradeName,
 		v_6_1_4.CreateUpgradeHandler(
-=======
-	if app.UpgradeKeeper.HasHandler(v_6_1_3.UpgradeName) {
-		panic(fmt.Sprintf("Cannot register duplicate upgrade handler '%s'", v_6_1_3.UpgradeName))
-	}
-	app.UpgradeKeeper.SetUpgradeHandler(
-		v_6_1_3.UpgradeName,
-		v_6_1_3.CreateUpgradeHandler(
->>>>>>> 2d1e37f962adb56bca35f67313a988c6d6611fd5
 			app.ModuleManager(),
 			app.configurator,
 			app.OracleKeeper,
