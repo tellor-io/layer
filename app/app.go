d2dfd08764777bc569fa066a5560276690a850f6
danflo27 <florentinodc@outlook.com>
2026-04-01 12:35:24 -0400

    remove one-time fix in preblock from height 1062219

--- app/app.go
+++ app/app.go
@@ -893,31 +893,6 @@ func (app *App) preBlocker(ph *ProposalHandler) func(ctx sdk.Context, _ *abci.Re
 		if err != nil {
 			return nil, err
 		}
-		if ctx.BlockHeight() == 1062219+1 {
-			bondedPoolBal := app.BankKeeper.GetBalance(ctx, authtypes.NewModuleAddress(stakingtypes.BondedPoolName), "loya")
-			vals, err := app.StakingKeeper.GetValidators(ctx, 7) // only 6 validators exist
-			if err != nil {
-				return nil, err
-			}
-			bondedtotal := math.ZeroInt()
-			notbondedtotal := math.ZeroInt()
-			for _, v := range vals {
-				if v.IsBonded() {
-					bondedtotal = bondedtotal.Add(v.Tokens)
-				} else {
-					notbondedtotal = notbondedtotal.Add(v.Tokens)
-				}
-			}
-			// transfer amount should be 1950000000
-			// "spendable balance 97171770000loya is smaller than 99121770000loya
-			transferAmt := bondedPoolBal.Amount.Sub(bondedtotal)
-			if transferAmt.Equal(math.NewInt(1950000000)) { // manually checked amount
-				err = app.BankKeeper.SendCoinsFromModuleToModule(ctx, stakingtypes.BondedPoolName, stakingtypes.NotBondedPoolName, sdk.NewCoins(sdk.NewCoin("loya", transferAmt)))
-				if err != nil {
-					return nil, err
-				}
-			}
-		}
 
 		changed := res.ConsensusParamsChanged
 
