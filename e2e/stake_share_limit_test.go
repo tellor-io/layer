ea902410f00b09bc563af6931a2c3e4fe21942f3
danflo27 <florentinodc@outlook.com>
2026-05-14 10:22:00 -0400

    e2e test

---
+++ e2e/stake_share_limit_test.go
@@ -0,0 +1,66 @@
+package e2e_test
+
+import (
+	"testing"
+
+	"github.com/strangelove-ventures/interchaintest/v8/chain/cosmos"
+	"github.com/strangelove-ventures/interchaintest/v8/ibc"
+	"github.com/strangelove-ventures/interchaintest/v8/testutil"
+	"github.com/stretchr/testify/require"
+	"github.com/tellor-io/layer/e2e"
+
+	"cosmossdk.io/math"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+)
+
+func TestDelegatorStakeShareLimit(t *testing.T) {
+	require := require.New(t)
+
+	cosmos.SetSDKConfig("tellor")
+
+	chain, ic, ctx := e2e.SetupChain(t, 2, 0)
+	defer ic.Close()
+
+	validators, err := e2e.GetValidators(ctx, chain)
+	require.NoError(err)
+	require.Len(validators, 2)
+
+	bondedValidators, err := chain.StakingQueryValidators(ctx, stakingtypes.BondStatusBonded)
+	require.NoError(err)
+	require.Len(bondedValidators, 2)
+
+	totalBonded := math.ZeroInt()
+	for _, validator := range bondedValidators {
+		totalBonded = totalBonded.Add(validator.Tokens)
+	}
+
+	validator0, err := chain.StakingQueryValidator(ctx, validators[0].ValAddr)
+	require.NoError(err)
+	require.True(
+		validator0.Tokens.MulRaw(10).GT(totalBonded.MulRaw(3)),
+		"test requires validator 0 to start above the 30% bonded stake limit",
+	)
+
+	require.NoError(chain.SendFunds(ctx, "faucet", ibc.WalletAmount{
+		Address: validators[0].AccAddr,
+		Amount:  math.NewInt(1_000_000),
+		Denom:   "loya",
+	}))
+	require.NoError(testutil.WaitForBlocks(ctx, 1, validators[0].Node))
+
+	delegateAmt := sdk.NewCoin("loya", math.OneInt())
+	_, err = validators[0].Node.ExecTx(
+		ctx,
+		validators[0].AccAddr,
+		"staking", "delegate",
+		validators[0].ValAddr,
+		delegateAmt.String(),
+		"--keyring-dir", validators[0].Node.HomeDir(),
+		"--gas", "500000",
+		"--fees", "10loya",
+	)
+	require.Error(err)
+	require.ErrorContains(err, "delegator bonded stake exceeds 30% of total bonded stake")
+}
