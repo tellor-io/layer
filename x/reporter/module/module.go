eded0862ebdff3f3f0450e16ecc2bce4e1e20f88
Cjpotter10 <caleb10joseph@gmail.com>
2026-05-28 08:47:08 -0500

    reset consensus version on reporter back to 1

--- x/reporter/module/module.go
+++ x/reporter/module/module.go
@@ -145,7 +145,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 // ConsensusVersion is a sequence number for state-breaking change of the module.
 // It should be incremented on each consensus-breaking change introduced by the module.
 // To avoid wrong/empty versions, the initial version should be set to 1.
-func (AppModule) ConsensusVersion() uint64 { return 3 }
+func (AppModule) ConsensusVersion() uint64 { return 1 }
 
 // EndBlock contains the logic that is automatically triggered at the end of each block.
 // The end block implementation is optional.
